3342e5f3c165b53c9023998c39745d52a8d4ec09
schinz <schinz@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2003-05-09 12:46:08 +0000

    - added a CVS Id tag, removed trailing ws
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@681 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- sources/scalac/ast/TreeList.java
+++ sources/scalac/ast/TreeList.java
@@ -3,11 +3,14 @@
 **  __\_ \/ /_/ / /__/ /_/ /\_ \       (c) 2002, LAMP/EPFL              **
 ** /_____/\____/\___/\____/____/                                        **
 **                                                                      **
-** $Id
 \*                                                                      */
+
+// $Id$
 
 package scalac.ast;
 
+/** List of trees.
+ */
 
 public final class TreeList {
     Tree[] trees;
@@ -22,7 +25,7 @@ public final class TreeList {
 	this(new Tree[4]);
 	len = 0;
     }
-    
+
     public void append(Tree tree) {
         if (len == trees.length) {
             Tree[] ts = new Tree[len * 2];
@@ -31,21 +34,21 @@ public final class TreeList {
         }
         trees[len++] = tree;
     }
-    
+
     public void append(Tree[] ts) {
         for (int j = 0; j < ts.length; j++)
             append(ts[j]);
     }
-    
+
     public void append(TreeList tl) {
         for (int j = 0; j < tl.len; j++)
             append(tl.trees[j]);
     }
-    
+
     public int length() {
         return len;
     }
-    
+
     public Tree get(int i) {
         return trees[i];
     }
@@ -53,17 +56,17 @@ public final class TreeList {
     public Tree first() {
         return trees[0];
     }
-    
+
     public Tree removeLast() {
         return trees[--len];
     }
-    
+
     public Tree[] toArray() {
         Tree[] ts = new Tree[len];
         System.arraycopy(trees, 0, ts, 0, len);
         return ts;
     }
-    
+
     public Tree[] copyTo(Tree[] ts) {
 	return copyTo(ts, 0);
     }
