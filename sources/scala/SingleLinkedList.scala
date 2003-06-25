c9b2733b2778f96356259886890a3c778262ae3d
michelou <michelou@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2003-06-25 14:17:52 +0000

    removed tabs
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@876 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- sources/scala/SingleLinkedList.scala
+++ sources/scala/SingleLinkedList.scala
@@ -4,42 +4,44 @@
 **  __\ \/ /__/ __ |/ /__/ __ |                                         **
 ** /____/\___/_/ |_/____/_/ | |                                         **
 **                          |/                                          **
-** $Id$
 \*                                                                      */
+
+// $Id$
+
 
 package scala;
 
 
 abstract class SingleLinkedList[A, This <: SingleLinkedList[A, This]]: This with Seq[A] {
-	
-	var elem: A = _;
-	
-	var next: This = _;
-	
-	def length: Int = 1 + (if (next == null) 0 else next.length);
-	
-	def append(that: This): Unit =
-		if (next == null) { next = that; } else next.append(that);
-	
-	def insert(that: This): Unit = if (that != null) {
-		that.append(next);
-		next = that;
-	}
+
+    var elem: A = _;
+
+    var next: This = _;
+
+    def length: Int = 1 + (if (next == null) 0 else next.length);
+
+    def append(that: This): Unit =
+        if (next == null) { next = that; } else next.append(that);
+
+    def insert(that: This): Unit = if (that != null) {
+        that.append(next);
+        next = that;
+    }
 	
     def apply(n: Int): A = {
         if (n == 0) elem
         else if (next == null) null
         else next.apply(n - 1);
     }
-    
+
     def at(n: Int): A = apply(n);
-    
+
     def get(n: Int): Option[A] = {
         if (n == 0) Some(elem)
         else if (next == null) None
         else next.get(n - 1);
     }
-    
+
     def elements: Iterator[A] = new Iterator[A] {
         var elems = SingleLinkedList.this;
         def hasNext = (elems != null);
@@ -49,7 +51,8 @@ abstract class SingleLinkedList[A, This <: SingleLinkedList[A, This]]: This with
             res;
         }
     }
-    
+
     def toList: List[A] =
         if (next == null) (elem :: Nil) else (elem :: next.toList);
+
 }
