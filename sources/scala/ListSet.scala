c2532f3b37ee154103784450276c1c3c770e8305
michelou <michelou@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2003-07-02 07:58:57 +0000

    removed tabs
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@895 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- sources/scala/ListSet.scala
+++ sources/scala/ListSet.scala
@@ -4,27 +4,31 @@
 **  __\ \/ /__/ __ |/ /__/ __ |                                         **
 ** /____/\___/_/ |_/____/_/ | |                                         **
 **                          |/                                          **
-** $Id$
 \*                                                                      */
+
+// $Id$
+
 
 package scala;
 
 /** I promise, there will be some documentation soon! :-) Matthias
  */
 class ListSet[A] extends MutableSet[A] {
-	protected var elems: List[A] = Nil;
 
-	def size: Int = elems.length;
-	
-	def contains(elem: A): Boolean = elems.contains(elem);
-	
-	def add(elem: A): Unit = if (!elems.contains(elem)) elems = elem :: elems;
-	
-	def remove(elem: A): Unit = { elems = elems.filter(e => e != elem); }
-	
-	def clear: Unit = { elems = Nil; }
-	
-	def elements: Iterator[A] = elems.elements;
-	
-	override def toList: List[A] = elems;
+    protected var elems: List[A] = Nil;
+
+    def size: Int = elems.length;
+
+    def contains(elem: A): Boolean = elems.contains(elem);
+
+    def add(elem: A): Unit = if (!elems.contains(elem)) elems = elem :: elems;
+
+    def remove(elem: A): Unit = { elems = elems.filter(e => e != elem); }
+
+    def clear: Unit = { elems = Nil; }
+
+    def elements: Iterator[A] = elems.elements;
+
+    override def toList: List[A] = elems;
+
 }
