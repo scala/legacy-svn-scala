0cbdcf35d8822d47ea601e8f0ac2941dfdb9baac
zenger <zenger@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2003-07-09 17:05:37 +0000

    Basic implementation of maps and sets.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@977 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- sources/scala/collection/immutable/ListMap.scala
+++ sources/scala/collection/immutable/ListMap.scala
@@ -4,28 +4,57 @@
 **  __\ \/ /__/ __ |/ /__/ __ |                                         **
 ** /____/\___/_/ |_/____/_/ | |                                         **
 **                          |/                                          **
+** $Id$
 \*                                                                      */
 
-// $Id$
+package scala.collection.immutable;
 
 
-package scala.collections.immutable;
+object ListMap {
+	def Empty[A, B] = new ListMap[A, B];
+}
 
 
-class ListMap[A, B] extends Map[A, B] with DefaultMapModel[A, B] {
+/** This class implements immutable maps using a list-based data
+ *  structure. Instances of <code>ListMap</code> represent
+ *  empty maps; they can be either created by calling the constructor
+ *  directly, or by applying the function <code>ListMap.Empty</code>.
+ *
+ *  @author  Matthias Zenger
+ *  @version 1.0, 09/07/2003
+ */
+class ListMap[A, B] with Map[A, B, ListMap[A, B]] {
 
-    var xs: List[Entry] = Nil;
-
-    def size: Int = xs.length;
-
-    override def clear: Unit = { xs = Nil; }
-
-    protected def findEntry(key: A) = xs find {e => e.key == key};
-
-    protected def addEntry(e: Entry) = { xs = e :: xs; }
-
-    def remove(key: A) = { xs = xs filter {e => e.key != key}; }
-
-    protected def entries = xs.elements;
-
+    def size: Int = 0;
+    
+    def get(key: A): Option[B] = None;
+	
+	def update(key: A, value: B): ListMap[A, B] = new Node(key, value);
+    
+    def -(key: A): ListMap[A, B] = this;
+    
+    def elements: Iterator[Pair[A, B]] = toList.elements;
+    
+    override def toList: List[Pair[A, B]] = Nil;
+    
+    protected class Node(key: A, value: B) extends ListMap[A, B] {
+        override def size: Int = ListMap.this.size + 1;
+       	override def isEmpty: Boolean = true;
+		override def apply(k: A): B = if (k == key) value else ListMap.this(k);
+		override def get(k: A): Option[B] =
+			if (k == key) Some(value) else ListMap.this.get(k);
+		override def update(k: A, v: B): ListMap[A, B] =
+			if (k == key) {
+				new ListMap.this.Node(k, v);
+			} else {
+				val y = ListMap.this.update(k, v); (new y.Node(key, value)): ListMap[A, B]
+			}
+		override def -(k: A): ListMap[A, B] =
+			if (k == key)
+				ListMap.this
+			else {
+				val y = ListMap.this - k; (new y.Node(key, value)): ListMap[A, B]
+			}
+		override def toList: List[Pair[A, B]] = Pair(key, value) :: ListMap.this.toList;
+    }
 }
