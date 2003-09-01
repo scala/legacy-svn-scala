25abe0ec0727d88b70c76ec8d72d58f3429845a9
buraq <buraq@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2003-09-01 15:02:58 +0000

    CodeFactory now creates trees with correct positions
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@1387 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- sources/scalac/transformer/matching/SequenceMatcher.java
+++ sources/scalac/transformer/matching/SequenceMatcher.java
@@ -227,7 +227,7 @@ public class SequenceMatcher extends PatternTool {
             assert body.length == pat.length;
             this._m = _m;
 
-            this.cf = new CodeFactory( unit, infer /*, _m.pos*/ );
+            this.cf = new CodeFactory( unit, infer, _m.pos );
 
             Type seqType = pat[ 0 ].type();
 
@@ -263,11 +263,13 @@ public class SequenceMatcher extends PatternTool {
                   defaultCase = cf.ThrowMatchError( Position.FIRSTPOS, _m.resultType );
             
             Tree newbody[] = doBinding ? addBindersToBodies( body ): body;
-            
+
+            // FIXME - CODE FOR TESTING TUPLE STUFF
+
             _m.tree = scalaAut.getMatcherSwitch( _m.selector, 
-                                                 defaultCase, 
-                                                 newbody,
-                                                 _m.resultType );
+						 defaultCase, 
+						 newbody,
+						 _m.resultType );
       } // construct (Matcher, Tree[], Tree[], Tree, boolean ) 
 
       /** constructor, invoked  by AlgebraicMatcher
