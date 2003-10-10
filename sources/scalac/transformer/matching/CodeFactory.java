0068bdcef95d0be3fe0e66e1bded576ba3eabe73
paltherr <paltherr@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2003-10-10 15:00:03 +0000

    - Replaced some use of "make" by "gen"
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@1621 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- sources/scalac/transformer/matching/CodeFactory.java
+++ sources/scalac/transformer/matching/CodeFactory.java
@@ -54,6 +54,18 @@ class CodeFactory extends PatternTool {
 	return defs.getType( Names.scala_List ).symbol() ;
     }
 
+    Symbol seqListSym_isEmpty() {
+	return seqListSym().lookup( ISEMPTY_N );
+    }
+
+    Symbol seqListSym_head() {
+	return seqListSym().lookup( HEAD_N );
+    }
+
+    Symbol seqListSym_tail() {
+	return seqListSym().lookup( TAIL_N );
+    }
+
     /*
     Symbol seqConsSym() {
 	return defs.getType( Names.scala_COLONCOLON ).symbol() ;
@@ -94,13 +106,25 @@ class CodeFactory extends PatternTool {
     Symbol newIterSym() {
 	Scope scp = iterableSym().members(); return scp.lookup( Names.elements );
     }
-    
-    
+
+    Symbol andSym() {
+        return defs.BOOLEAN_CLASS.lookup(AND_N);
+    }
+
+    Symbol orSym() {
+        return defs.BOOLEAN_CLASS.lookup(OR_N);
+    }
+
+    Symbol failSym() {
+        return defs.SCALA_CLASS.lookup(MATCHERROR_N).lookup(FAIL_N);
+    }
+
+
     public CodeFactory( Unit unit, Infer infer, int pos ) {
 	super( unit, infer );
 	this.pos = pos;
     }
-    
+
     // --------- these are new
 
     /** If  ... pos, type is copied from thenBody
@@ -109,54 +133,42 @@ class CodeFactory extends PatternTool {
 	assert cond != null:"cond is null";
 	assert thenBody != null:"thenBody is null";
 	assert elseBody != null:"elseBody is null";
-	return make.If( thenBody.pos,
-			cond,
-			thenBody, 
-			elseBody ).setType( elseBody.type() );
+	return gen.If( thenBody.pos, cond, thenBody, elseBody );
     }
 
     /** a faked switch statement
-     */ 
-    Tree Switch( Tree selector, 
-		 Tree condition[], 
-		 Tree body[], 
+     */
+    Tree Switch( Tree selector,
+		 Tree condition[],
+		 Tree body[],
 		 Tree defaultBody ) {
 	assert selector != null:"selector is null";
 	assert condition != null:"cond is null";
 	assert body != null:"body is null";
 	assert defaultBody != null:"defaultBody is null";
 	Tree result = defaultBody;
-	
-	for( int i = condition.length-1; i >= 0; i-- ) 
+
+	for( int i = condition.length-1; i >= 0; i-- )
 	    result = If( condition[ i ],
 			 body[ i ],
 			 result
 			 ).setType( result.type );
-	
+
 	return result ;
     }
-	
-    /** code for a real switch statement
-     */ 
-	Tree Switch(Tree selector,
-	            int[] tags,
-	            Tree[] bodies,
-	            Tree defaultBody) {
-		return make.Switch(selector.pos, selector, tags, bodies, defaultBody);            
-	}
-	
+
     /** returns `List[ elemType ]' */
       Type SeqListType( Type elemType ) {
-            return Type.TypeRef( defs.SCALA_TYPE, 
-                                 seqListSym(), 
+            return Type.TypeRef( defs.SCALA_TYPE,
+                                 seqListSym(),
                                  new Type[] { elemType });
       }
 
     /** returns  `List[ Tuple2[ scala.Int, <elemType> ] ]' */
       Type SeqTraceType( Type elemType ) {
-	  Type t = Type.TypeRef( defs.SCALA_TYPE, 
+	  Type t = Type.TypeRef( defs.SCALA_TYPE,
                                  seqListSym(),
-                                 new Type[] { pairType( defs.INT_TYPE,  
+                                 new Type[] { pairType( defs.INT_TYPE,
 							elemType ) });
 	  //System.err.println("CodeFactory::SeqTraceType -"+ t );
 	  return t;
@@ -166,7 +178,7 @@ class CodeFactory extends PatternTool {
     Type _seqIterType( Type elemType ) {
 	Symbol seqIterSym = defs.getType( Names.scala_Iterator ).symbol();
 
-	return Type.TypeRef( defs.SCALA_TYPE, seqIterSym(), 
+	return Type.TypeRef( defs.SCALA_TYPE, seqIterSym(),
 			     new Type[] { elemType });
     }
 
@@ -175,14 +187,14 @@ class CodeFactory extends PatternTool {
 	Symbol newIterSym = newIterSym();
 	Tree t1 = gen.Select( pos, seqObj, newIterSym)
 	    .setType( Type.MethodType(new Symbol[] {},_seqIterType( elemType )));
-	
+
 	Tree theIterator = gen.Apply(seqObj.pos,
 				     t1,
 				     Tree.EMPTY_ARRAY)
 	    .setType( _seqIterType( elemType ) );
-	
+
 	return theIterator;
-	
+
     }
 
     /** returns code `<seqObj>.elements'
@@ -192,75 +204,18 @@ class CodeFactory extends PatternTool {
 	return newIterator( seqObj, getElemType_Sequence( seqObj.type() ));
     }
 
-    /** FIXME - short type
-      Tree ignoreValue1( Type asType ) {
-            if( asType.isSameAs(defs.BYTE_TYPE ))
-                  return make.Literal(pos, new Integer( 0 ))
-                        .setType( defs.INT_TYPE );
-            else if( asType.isSameAs( defs.CHAR_TYPE ))
-                  return make.Literal(pos, new Character( 'a' ))
-                        .setType( defs.CHAR_TYPE );
-            else if( asType.isSameAs(defs.SHORT_TYPE ))
-		return make.Literal(pos, new Integer( 0 ))//Short
-                        .setType( defs.SHORT_TYPE );
-            else if( asType.isSameAs(defs.INT_TYPE ))
-                  return gen.mkIntLit( pos, 0 );
-            else if( asType.isSameAs(defs.LONG_TYPE ))
-                  return make.Literal(pos, new Long( 0 ))
-                        .setType( defs.LONG_TYPE );
-            else if( asType.isSameAs(defs.FLOAT_TYPE ))
-                  return make.Literal(pos, new Float( 0 ))
-                        .setType( defs.FLOAT_TYPE );
-            else if( asType.isSameAs(defs.DOUBLE_TYPE ))
-                  return make.Literal(pos, new Double( 0 ))
-                        .setType( defs.DOUBLE_TYPE );
-            else if( asType.isSameAs(defs.BOOLEAN_TYPE ))
-                  return gen.mkBooleanLit(pos, false);
-            else if( asType.isSameAs(defs.STRING_TYPE ))
-                  return make.Literal(pos, "")
-                        .setType( defs.STRING_TYPE );
-	    else 
-		return Null( asType );
-	    //throw new ApplicationError("don't know how to handle "+asType); 
-      }
-     */
-
-    /** code `null'
-    Tree Null( Type asType ) {
-	return gen.Ident(pos, defs.NULL );
-    }
-     */
-    
-    // the caller needs to set the type !
-    Tree  _applyNone( Tree arg ) {
-	return make.Apply(pos, arg, Tree.EMPTY_ARRAY );
-    }
-
     /** code `Nil'
         Tree _seqTraceNil( Type elemType ) {
 	return newSeqNil( null );
     }
      */
-    
 
-      /** creates an scala.Int constant
-      Tree Int( int val ) {
-            return Int( new Integer( val ));
-      }
-
-      Tree Int( Integer valI ) {
-            return make.Literal( pos, valI )
-                  .setType( defs.INT_TYPE );
-
-      }
-       */
-            
       //                       `SeqCons[ elemType ]'
     /*
-      
+
       Type _seqConsType( Type elemType ) {
-            return Type.TypeRef( defs.SCALA_TYPE, 
-                                 seqConsSym(), 
+            return Type.TypeRef( defs.SCALA_TYPE,
+                                 seqConsSym(),
                                  new Type[] { elemType });
       }
 
@@ -295,49 +250,40 @@ class CodeFactory extends PatternTool {
 	//System.err.println("getElemType_Iterator("+tpe+")");
 
 	Type tpe1 = tpe.widen().baseType( iteratorSym() );
-	
+
 	switch( tpe1 ) {
 	case TypeRef(_,_,Type[] args):
 	    return args[ 0 ];
 	default:
 	    throw new ApplicationError("arg "+tpe+" not subtype of Iterator[ A ]");
 	}
-	
+
     }
 
     /** `it.next()'
      */
     public Tree _next( Tree iter ) {
-	Type elemType = getElemType_Iterator( iter.type() );
-	Symbol nextSym = seqIterSym_next(); 
-	return _applyNone( gen.Select( iter, nextSym )).setType( elemType );
+	return gen.mkApply__(gen.Select(iter, seqIterSym_next()));
     }
-    
+
     /** `it.hasNext()'
      */
     public Tree _hasNext( Tree iter ) {
-
-	Symbol hasNextSym = seqIterSym_hasNext(); 
-
-	return _applyNone( gen.Select( iter, hasNextSym )).setType( defs.BOOLEAN_TYPE );
+	return gen.mkApply__(gen.Select(iter, seqIterSym_hasNext()));
     }
-      
+
     /** `!it.hasCur()'
      */
     public Tree _not_hasNext( Tree iter ) {
-	return _applyNone( gen.Select( _hasNext( iter ), notSym ))
-	    .setType( defs.BOOLEAN_TYPE );
+	return gen.mkApply__(gen.Select(_hasNext(iter), notSym));
     }
 
       /** `trace.isEmpty'
        */
       public Tree isEmpty( Tree iter ) {
-	  Scope scp = seqListSym()/*TraceSym()*/.members();
-            Symbol isEmptySym = scp.lookup ( ISEMPTY_N );
-            return _applyNone( gen.Select( iter, isEmptySym ))
-                  .setType( defs.BOOLEAN_TYPE );
+          return gen.mkApply__(gen.Select(iter, seqListSym_isEmpty()));
       }
-      
+
     Tree SeqTrace_headElem( Tree arg ) { // REMOVE SeqTrace
 	Scope scp = seqListSym().members();
 	Symbol headSym = scp.lookup ( HEAD_N );
@@ -346,11 +292,11 @@ class CodeFactory extends PatternTool {
 	Symbol leftSym = scp.lookup ( RIGHT_N );
 	assert leftSym != Symbol.NONE;
 	Tree t =  gen.Apply( gen.Select( pos, arg, headSym ),
-			     Tree.EMPTY_ARRAY );            
+			     Tree.EMPTY_ARRAY );
 	return gen.Apply( gen.Select( pos, t, leftSym ),
-			  Tree.EMPTY_ARRAY );            
+			  Tree.EMPTY_ARRAY );
     }
-    
+
     Tree SeqTrace_headState( Tree arg ) { // REMOVE SeqTrace
 	Scope scp = seqListSym().members();
 	Symbol headSym = scp.lookup ( HEAD_N );
@@ -361,9 +307,9 @@ class CodeFactory extends PatternTool {
 	Tree t =  gen.Apply( gen.Select( pos, arg, headSym ),
 			     Tree.EMPTY_ARRAY );
 	t =  gen.Apply( gen.Select( pos, t, leftSym ),
-			Tree.EMPTY_ARRAY );            
+			Tree.EMPTY_ARRAY );
 	return t;
-	
+
     }
 
     Tree SeqTrace_tail( Tree arg ) {
@@ -372,9 +318,9 @@ class CodeFactory extends PatternTool {
 	assert tailSym != Symbol.NONE;
 	//System.err.println( "CodeFactory::SeqTrace_tail " + tailSym +" "+ tailSym.type() );
 	return gen.Apply( gen.Select( pos, arg, tailSym ),
-			  Tree.EMPTY_ARRAY );            
+			  Tree.EMPTY_ARRAY );
     }
-    
+
     /** `<seqlist>.head()'
      */
     Tree SeqList_head( Tree arg ) {
@@ -384,15 +330,15 @@ class CodeFactory extends PatternTool {
 	return gen.Apply( gen.Select( pos, arg, headSym ),
 			  Tree.EMPTY_ARRAY );
     }
-    
+
     /** return the analyzed type
      */
     public Type typeOf(Symbol sym) {
         return sym.type();
         //return sym.typeAt(unit.global.ANALYZER_PHASE.id);
     }
-    
-    
+
+
     protected Tree Block(int pos, Tree[] ts, Type tpe) {
         if (ts.length == 1)
             return ts[0];
@@ -404,7 +350,7 @@ class CodeFactory extends PatternTool {
 		System.arraycopy(ts0, 0, ts1, ts.length - 1, ts0.length);
 		return Block(pos, ts1, tpe);
             }
-        return make.Block(pos, ts).setType(tpe);
+        return gen.Block(pos, ts);
     }
 
      // unused
@@ -415,7 +361,7 @@ class CodeFactory extends PatternTool {
        }
        return gen.Apply(tree.pos, gen.Select(tree, notSym));
        }
-    
+
     protected Tree And(Tree left, Tree right) {
         switch (left) {
 	case Literal(Object value):
@@ -425,16 +371,9 @@ class CodeFactory extends PatternTool {
 	case Literal(Object value):
 	    if (((Boolean)value).booleanValue()) return left;
         }
-        Symbol fun = left.type.lookup(AND_N);
-        return make.Apply(
-			  left.pos,
-			  make.Select(
-				      left.pos,
-				      left,
-				      AND_N).setType(typeOf(fun)).setSymbol(fun),
-			  new Tree[]{right}).setType(defs.BOOLEAN_TYPE);
+        return gen.mkApply_V(gen.Select(left, andSym()), new Tree[]{right});
     }
-    
+
     protected Tree Or(Tree left, Tree right) {
         switch (left) {
 	case Literal(Object value):
@@ -444,109 +383,28 @@ class CodeFactory extends PatternTool {
 	case Literal(Object value):
 	    if (!((Boolean)value).booleanValue()) return left;
         }
-        Symbol fun = left.type.lookup(OR_N);
-        return make.Apply(
-			  left.pos,
-			  make.Select(
-				      left.pos,
-				      left,
-				      OR_N).setType(typeOf(fun)).setSymbol(fun),
-			  new Tree[]{right}).setType(defs.BOOLEAN_TYPE);
+        return gen.Apply(gen.Select(left, orSym()), new Tree[]{right});
     }
-    
-    protected Tree Is(Tree tree, Type type) {
-        return 
-            make.Apply(
-		       tree.pos,
-		       make.TypeApply(
-				      tree.pos,
-				      make.Select(
-						  tree.pos,
-						  tree,
-						  defs.IS.name).setType(typeOf(defs.IS)).setSymbol(defs.IS),
-				      new Tree[]{gen.mkType(tree.pos, type)})
-		       .setType(Type.MethodType(Symbol.EMPTY_ARRAY, defs.BOOLEAN_TYPE)),
-		       Tree.EMPTY_ARRAY).setType(defs.BOOLEAN_TYPE);
-    }
-    
-    protected Tree As(Tree tree, Type type) {
-        return
-            make.Apply(
-		       tree.pos,
-		       make.TypeApply(
-				      tree.pos,
-				      make.Select(
-						  tree.pos,
-						  tree,
-						  defs.AS.name).setType(typeOf(defs.AS)).setSymbol(defs.AS),
-				      new Tree[]{gen.mkType(tree.pos, type)})
-		       .setType(Type.MethodType(Symbol.EMPTY_ARRAY, type)),
-		       Tree.EMPTY_ARRAY).setType(type);
-    }
-    
+
     protected Tree Equals(Tree left, Tree right) {
-        Symbol fun = left.type.lookup(EQUALS_N);
-        switch (typeOf(fun)) {
-	case OverloadedType(Symbol[] alts, Type[] alttypes):
-	    //System.out.println("**** " + left.type);
-	    Tree t = make.Select(left.pos, left, EQUALS_N);
-	    //for (int i = 0; i < alttypes.length; i++)
-	    //    System.out.println(alts[i] + ": " + alttypes[i]);
-	    infer.methodAlternative(t, alts, alttypes,
-				    new Type[]{right.type}, defs.BOOLEAN_TYPE);
-	    return make.Apply(left.pos, t, new Tree[]{right}).setType(defs.BOOLEAN_TYPE);
-	default:
-	    //System.out.println("#### " + left.type + ": " + fun);
-	    return make.Apply(
-			      left.pos,
-			      make.Select(
-					  left.pos,
-					  left,
-					  EQUALS_N).setType(typeOf(fun)).setSymbol(fun),
-			      new Tree[]{right}).setType(defs.BOOLEAN_TYPE);
-        }
+        Symbol fun = unit.global.definitions.EQEQ;
+        return gen.mkApply_V(gen.Select(left, fun), new Tree[]{right});
     }
-    
+
     protected Tree ThrowMatchError(int pos, Type type) {
-        Symbol matchErrorModule = defs.SCALA.members().lookup(MATCHERROR_N);
-        outer: switch (typeOf(matchErrorModule)) {
-	case OverloadedType(Symbol[] alts, Type[] alttypes):
-	    for (int i = 0; i < alts.length; i++)
-		switch (alttypes[i]) {
-		case TypeRef(_, _, _):
-		    matchErrorModule = alts[i];
-		    break outer;
-		}
-        }
-        Symbol failMethod = typeOf(matchErrorModule).lookup(FAIL_N);
-        return
-	    make.Apply(
-		       pos,
-		       make.TypeApply(
-				      pos,
-				      make.Select(
-						  pos,
-						  make.Select(
-							      pos,
-							      make.Ident(pos, Names.scala).setType(typeOf(defs.SCALA)).setSymbol(defs.SCALA),
-							      MATCHERROR_N)
-						  .setSymbol(matchErrorModule)
-						  .setType(typeOf(matchErrorModule)),
-						  FAIL_N).setType(typeOf(failMethod)).setSymbol(failMethod),
-				      new Tree[]{gen.mkType(pos, type)})
-		       .setType(((Type.PolyType) typeOf(failMethod)).result.subst(
-										  typeOf(failMethod).typeParams(),
-										  new Type[]{type})),
-		       new Tree[]{
-			   make.Literal(pos, unit.toString()).setType(defs.STRING_TYPE),
-			   make.Literal(pos, new Integer(Position.line(pos))).setType(defs.INT_TYPE)
-		       }).setType(type);
+        return gen.mkApplyTV(
+			     gen.mkRef(pos, failSym()),
+                             new Tree[]{gen.mkType(pos, type)},
+                             new Tree[]{
+                                 gen.mkStringLit(pos, unit.toString()),
+                                 gen.mkIntLit(pos, Position.line(pos))
+                             });
     }
- 
+
 
     Type pairType( Type left, Type right ) {
-	return Type.TypeRef( defs.SCALA_TYPE, 
-			     tuple2Sym() , 
+	return Type.TypeRef( defs.SCALA_TYPE,
+			     tuple2Sym() ,
 			     new Type[] { left, right } );
     }
 
@@ -554,7 +412,7 @@ class CodeFactory extends PatternTool {
  	return gen.New(gen.mkPrimaryConstr(pos, tuple2Sym(),
 			                   new Type[] { left.type(), right.type() },
 			                   new Tree[] { left, right }));
-	
+
     }
-   
+
 }
