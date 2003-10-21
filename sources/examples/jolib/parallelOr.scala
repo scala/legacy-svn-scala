aba3341e704aeb956c61220dab9113aa82de19db
cremet <cremet@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2003-10-21 15:08:19 +0000

    *** empty log message ***
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@1708 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- sources/examples/jolib/parallelOr.scala
+++ sources/examples/jolib/parallelOr.scala
@@ -12,27 +12,27 @@ object or extends Join {
   
   rules(
     Pair(List(res, res1), { case List(r @ res.C(), res1.C(b)) =>
-      if (b) r.set(b) else r.set(res1False.send(res1False.C())) }),
-
+      if (b) r.set(b) else r.set(res1False(res1False.C())) }),
+    
     Pair(List(res, res2), { case List(r @ res.C(), res2.C(b)) =>
-      if (b) r.set(b) else r.set(res2False.send(res2False.C())) }),
-
+      if (b) r.set(b) else r.set(res2False(res2False.C())) }),
+    
     Pair(List(res1False, res2),  { case List(r @ res1False.C(), res2.C(b)) =>
       r.set(b) }),
-
+    
     Pair(List(res2False, res1),  { case List(r @ res2False.C(), res1.C(b)) =>
       r.set(b) })
   );
   
   def apply(def b1: boolean, def b2: boolean): boolean = {
-    concurrent.ops.spawn(res1.send(res1.C(b1)));
-    concurrent.ops.spawn(res2.send(res2.C(b2)));
-    res.send(res.C())
+    concurrent.ops.spawn(res1(res1.C(b1)));
+    concurrent.ops.spawn(res2(res2.C(b2)));
+    res(res.C())
   }
 }
 
 object parallelOr {
-
+  
   def main(args: Array[String]): unit = {
     def loop: boolean = { while (true) {}; true };
     System.out.println("true || true = " + or(true, true));
