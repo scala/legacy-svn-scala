047a68be8284939167015a54ecb0a4621af129f0
stenman <stenman@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2003-12-01 15:05:34 +0000

    updated to not use 'this'
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@2072 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- test/files/run/map_test.scala
+++ test/files/run/map_test.scala
@@ -4,8 +4,8 @@ import scala.collection.immutable.ListMap;
 import scala.collection.immutable.Order;
 
 object Test with Executable {
-    val intOrder = 
-	new Order((x:int,y:int) => x < y, (x:int,y:int) => x == y);     
+    val intOrder =
+	new Order((x:int,y:int) => x < y, (x:int,y:int) => x == y);
 
     test1();
     test2();
@@ -16,14 +16,14 @@ object Test with Executable {
     def test1() = {
 	val myMap:TreeMap[int,String] = new TreeMap(intOrder);
         test_map(myMap);
-    }   
+    }
 
     def test2() = {
 	val myMap:ListMap[int,String] = new ListMap;
 	test_map(myMap);
     }
 
-    def test_map[This <: Map[int,String,This]](myMap:Map[int,String,This]) = {
+    def test_map(myMap:Map[int,String]) = {
 	val map1 = myMap.update(42,"The answer");
 	val map2 = map1.update(17,"A small random number");
 	val map3 = map2.update(666,"A bigger random number");
@@ -37,7 +37,7 @@ object Test with Executable {
 	}
 	i = 0;
 	while(i < 4712) {
-	    if(map.isDefinedAt(i))	
+	    if(map.isDefinedAt(i))
 		Console.print(i + "->" + map(i) + " ");
 	    i = i + 1;
 	}
