11f72c98f1e4cce8679ac85649f10480448755d5
buraq <buraq@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2004-01-07 14:29:43 +0000

    cleanup
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@2288 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- sources/scalac/transformer/matching/CollectVariableTraverser.java
+++ sources/scalac/transformer/matching/CollectVariableTraverser.java
@@ -9,12 +9,11 @@ import java.util.Vector;
 class CollectVariableTraverser extends VariableTraverser {
 
       protected Vector vars;
-      
+
       boolean isVariableName( Name name ) {
             return ( name.toString().indexOf("$") == -1 )
                   && super.isVariableName( name );
       }
-      
       
       void handleVariableSymbol( Symbol sym ) {
             vars.add( sym );
