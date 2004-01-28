eeff516694ab01f5c783b5be1284fcc7450fe9c7
buraq <buraq@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2004-01-28 15:13:42 +0000

    bugfix in PatternMatcher. reusing node tests has to handle names
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@2505 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- sources/scalac/transformer/matching/CaseEnv.java
+++ sources/scalac/transformer/matching/CaseEnv.java
@@ -41,7 +41,20 @@ class CaseEnv {
     
     protected ValDef[] boundVars = new ValDef[4];
     protected int numVars = 0;
-    
+
+      /** substitutes a symbol on the right hand side of a ValDef
+       */
+      public void substitute( Symbol oldSym, Tree newInit ) {
+            int i = 0; 
+            while( i < numVars) {
+                  if( boundVars[ i ].rhs.symbol() == oldSym ) {
+                        boundVars[ i ].rhs = newInit;
+                        return;
+                  }
+                  i++ ;
+            }
+      }
+
     public void newBoundVar(Symbol sym, Type type, Tree init) {
 	sym.setOwner( owner ); // FIXME should be corrected earlier
 	if (numVars == boundVars.length) {
