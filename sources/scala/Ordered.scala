163a4c44cfd0af2c7d5e0bd459384596b8ae71fb
zenger <zenger@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2004-04-29 10:38:17 +0000

    *** empty log message ***
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@3082 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- sources/scala/Ordered.scala
+++ sources/scala/Ordered.scala
@@ -11,6 +11,9 @@ package scala;
 
 
 /** A trait for totally ordered data.
+ *
+ *  @author  Martin Odersky
+ *  @version 1.0, 23/04/2004
  */
 trait Ordered[+a] {
 
@@ -21,7 +24,7 @@ trait Ordered[+a] {
    *  <code>x &gt; 0</code>    iff    <code>this &gt; that</code>
    */
   def compareTo [b >: a <% Ordered[b]](that: b): int;
-
+  
   def <  [b >: a <% Ordered[b]](that: b): boolean = (this compareTo that) <  0;
 
   def >  [b >: a <% Ordered[b]](that: b): boolean = (this compareTo that) >  0;
@@ -30,4 +33,3 @@ trait Ordered[+a] {
 
   def >= [b >: a <% Ordered[b]](that: b): boolean = (this compareTo that) >= 0;
 }
-
