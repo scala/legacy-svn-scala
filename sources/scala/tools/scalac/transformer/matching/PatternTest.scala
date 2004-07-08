641d3d795a0e181ab7080030f722c45588a1eeee
buraq <buraq@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2004-07-08 17:05:14 +0000

    regexp lib, nodeSeq, attrSeq changes, test cases updated
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@3380 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- sources/scala/tools/scalac/transformer/matching/PatternTest.scala
+++ sources/scala/tools/scalac/transformer/matching/PatternTest.scala
@@ -1,12 +1,19 @@
-package scala.tools.scalac.transformer.matching ;
+import scalac.ast.Tree ;
+import scalac.atree.AConstant ;
+import scalac.symtab.Type ;
+import scala.util.alphabet.Alphabet;
 
-abstract class PatternTest ;
+package scala.tools.scalac.transformer.matching {
 
-/** test for patterns _:T      */ 
-case class TypeTest extends PatternTest ;
+  abstract class PatternTest extends Alphabet ;
+  
+  case class  Constructor( tpe:Type ) extends PatternTest ;
+  case object WildCard                extends PatternTest;
+  
+  abstract class LeafTest extends PatternTest ;
+  
+  case class EqualsConstant( const:AConstant ) extends LeafTest ;
+  case class EqualsValue( value:Tree )         extends LeafTest ;
+  case class TypeTest( tpe:Type )        extends LeafTest;
 
-/** test for patterns A(...)   */ 
-case class CaseTest extends PatternTest ;
-
-/** test for constant patterns */ 
-case class EqTest extends PatternTest ;
+}
