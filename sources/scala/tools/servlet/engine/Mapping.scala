952a43e0f7f0a461be6889d7a64c08416c869340
buraq <buraq@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2004-07-27 15:49:43 +0000

    hello
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@3478 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- sources/scala/tools/servlet/engine/Mapping.scala
+++ sources/scala/tools/servlet/engine/Mapping.scala
@@ -1,24 +1,7 @@
 package scala.tools.servlet.engine;
 
-import java.io._;
-
-class Mapping(){
+trait Mapping {
   
-  val map = new File(http.HTTP.SERVER_LOCATION,http.HTTP.translateFilename("map.prop"));
-  val myProp = new java.util.Properties();
+  def switch(uri: String): String;
   
-  try{
-    myProp.load(new FileInputStream(map));
-  }
-  
-  catch{    
-    case e:FileNotFoundException  =>  System.out.println("FileNotFoundException ");
-    case e:IOException =>  System.out.println("IOException");
-    case e: IllegalArgumentException =>  System.out.println(" IllegalArgumentException");
-  }
-  
-  def switch(original:String):String={ 
-    myProp.getProperty(original);
-  }
- 
 }
