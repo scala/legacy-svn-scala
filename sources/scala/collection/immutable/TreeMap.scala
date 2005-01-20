a6fbc2674d046d63e54688b99805ea380fea969f
schinz <schinz@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2005-01-20 12:34:18 +0000

    - rewrote Tree significantly in order to make it work with run-time   types (the previous implementation was buggy, and worked only   because pattern-matching was not precise enough), - removed the entryKey method, now useless, - made most of the interface protected, to be consistent with the rest   of the library.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@3885 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- sources/scala/collection/immutable/TreeMap.scala
+++ sources/scala/collection/immutable/TreeMap.scala
@@ -20,53 +20,48 @@ object TreeMap {
  *  @version 1.1, 03/05/2004
  */
 class TreeMap[A <% Ordered[A], B] extends Tree[A, Pair[A, B]] with Map[A, B] {
-    
-    override type This = TreeMap[A, B];
-    
+
+    override protected type This = TreeMap[A, B];
+    override protected def getThis: This = this;
+
     /** A factory to create empty maps of the same type of keys.
      */
     def empty[C] = new TreeMap[A, C];
- 
-    /** Returns the key of an entry.
-     *  This method has to be defined by concrete implementations
-     *  of the class.
-     */
-    override def entryKey(entry:Pair[A,B]) = entry._1;
- 
+
     /** Creates a new TreeMap from a GBTree and its size.
      */
     protected def New(sz:Int,t:aNode):This = new TreeMap[A,B] {
         override def size=sz;
-        override protected val tree:aNode=t;
+        override protected def tree:aNode=t;
     }
 
-    /** A new TreeMap with the entry added is returned, 
+    /** A new TreeMap with the entry added is returned,
      *  if key is <em>not</em> in the TreeMap, otherwise
      *  the key is updated with the new entry.
      */
-    def update(key:A, value:B) = update_or_add(key,Pair(key,value));
-    
-    /** A new TreeMap with the entry added is returned, 
+    def update(key:A, value:B) = updateOrAdd(key, Pair(key, value));
+
+    /** A new TreeMap with the entry added is returned,
      *  assuming that key is <em>not</em> in the TreeMap.
-     */  
-    def insert(key:A,value:B) = add(key,Pair(key,value));
- 
+     */
+    def insert(key:A,value:B) = add(key, Pair(key, value));
+
     /** Removes the key from the TreeMap.
      */
-    def -(key:A) = delete_any(key);  
- 
+    def -(key:A) = deleteAny(key);
+
     /** Check if this map maps <code>key</code> to a value and return the
-     *  value if it exists. 
+     *  value if it exists.
      *
      *  @param  key     the key of the mapping of interest
      *  @return the value of the mapping, if it exists
      */
-    override def get(key:A) = 
-      findValue(key).match {
-        case Some(Pair(_,value:B)) => Some(value);
-        case _ => None;
+    override def get(key: A): Option[B] =
+      findValue(key) match {
+        case Some(Pair(_, value: B)) => Some(value)
+        case _ => None
       }
- 
+
     /** Retrieve the value which is associated with the given key. This
      *  method throws an exception if there is no mapping from the given
      *  key to a value.
@@ -76,20 +71,21 @@ class TreeMap[A <% Ordered[A], B] extends Tree[A, Pair[A, B]] with Map[A, B] {
      *  @throws Error("key not found").
      */
     override def apply(key:A):B = tree.apply(key)._2;
-  
+
     /** Creates a list of all (key, value) mappings.
      *
      *  @return    the list of all mappings
      */
-    override def toList:List[Pair[A,B]] = tree.toList(scala.Nil);
- 
+    override def toList: List[Pair[A,B]] =
+      tree.toList(scala.Nil) map (._2);
+
     /** Creates a new iterator over all elements contained in this
      *  object.
-     *  
+     *
      *  @return the new iterator
      */
-    def elements:Iterator[Pair[A,B]] = entries;
- 
+    def elements: Iterator[Pair[A,B]] = entries;
+
     /** Compares two maps structurally; i.e. checks if all mappings
      *  contained in this map are also contained in the other map,
      *  and vice versa.
@@ -97,14 +93,13 @@ class TreeMap[A <% Ordered[A], B] extends Tree[A, Pair[A, B]] with Map[A, B] {
      *  @return    true, iff both maps contain exactly the same mappings.
      */
     override def equals(obj: Any): Boolean =
-        if (obj.isInstanceOf[scala.collection.Map[A, B]]) {
-            val that = obj.asInstanceOf[scala.collection.Map[A, B]];
-            if (size != that.size) false else elements.forall {
-                case Pair(key, value) => that.get(key) match {
-                  case None => false;
-                  case Some(v) => v == value;
-                }
-            }
-        } else
-          false;
+      obj.isInstanceOf[scala.collection.Map[A, B]] && {
+        val that = obj.asInstanceOf[scala.collection.Map[A, B]];
+        size == that.size && elements.forall {
+          case Pair(key, value) => that.get(key) match {
+            case None => false;
+            case Some(v) => v == value;
+          }
+        }
+      };
 }
