5ed745263aa55c04f8742a25902ef601cfd20b3e
buraq <buraq@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2005-05-04 17:37:37 +0000

    XML library, DTD validation (changes in parser, additions to scala.xml.dtd)
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@4140 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- sources/scala/xml/dtd/DTD.scala
+++ sources/scala/xml/dtd/DTD.scala
@@ -1,5 +1,7 @@
 package scala.xml.dtd;
 
+import scala.collection.mutable.{ HashMap, Map }
+
 /** a document type declaration */
 abstract class DTD {
  
@@ -8,16 +10,39 @@ abstract class DTD {
   def notations: Seq[NotationDecl] = Nil;
   
   def unparsedEntities: Seq[EntityDecl] = Nil;
+
+  var elem: Map[String, ElemDecl] = 
+    new HashMap[String, ElemDecl]();
+
+  var attr: Map[String, AttListDecl] = 
+    new HashMap[String, AttListDecl]();
   
-  var decls: List[MarkupDecl] = Nil;
+
+  var decls: List[Decl] = Nil;
 
   //def getElemDecl(elem:String): ElemDecl;
 
   //def getAttribDecl(elem: String, attr: String): AttrDecl;
 
   override def toString() = {
-    val s = super.toString();
-    "[DTD "+s.substring(s.indexOf('@'), s.length())+"]";
+    val sb = new StringBuffer();
+    sb.append("DTD [\n");
+    if(null != externalID)
+      sb.append(externalID.toString()).append('\n');
+    for(val d <- decls) 
+      sb.append(d.toString()).append('\n');
+    sb.append("]").toString()
   }
 
+  /** creates fresh type symbols from declarations */
+  def createTypeSymbols(): Unit = {
+    elem.clear;
+    /*
+    for(val d <- decl)
+      d.match {
+        case ElemDecl(name, contentModel) =>
+          elementType.update(name, new ElementType(name, contentModel)
+      }
+      */
+  }
 }
