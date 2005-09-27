ff6344420d25f74f62bcfc2437710e292def2b8b
michelou <michelou@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2005-09-27 14:59:20 +0000

    - added Moez's changes for Scala interpreter.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@4543 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- sources/scala/tools/nsc/ast/parser/SyntaxAnalyzer.scala
+++ sources/scala/tools/nsc/ast/parser/SyntaxAnalyzer.scala
@@ -6,7 +6,7 @@
 package scala.tools.nsc.ast.parser;
 
 /** An nsc sub-component.
- */ 
+ */
 abstract class SyntaxAnalyzer extends SubComponent with Parsers with Scanners {
   val phaseName = "parser";
   def newPhase(prev: Phase): StdPhase = new ParserPhase(prev);
@@ -16,5 +16,10 @@ abstract class SyntaxAnalyzer extends SubComponent with Parsers with Scanners {
       unit.body = new Parser(unit).parse();
     }
   }
+  //Moez addition. I wished not to add/modify here, but the fact that Parsers
+  // are NOT accessible (because of Parsers' self type) except in SyntaxAnalyzer
+  // had bitten me, and thus I had to add the following code here.
+  def interpreterParse(unit: global.CompilationUnit): List[global.Tree] =
+    new Parser(unit).templateStatSeq()
 }
 
