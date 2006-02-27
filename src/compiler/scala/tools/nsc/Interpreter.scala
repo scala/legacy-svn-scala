c6d8cfd085ea890b5bc02566ea9d46dec4c813bf
spoon <spoon@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2006-02-27 17:15:01 +0000

    allocate new line numbers more eagerly, thus consistently avoiding reuse of existing .class files
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@5864 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/tools/nsc/Interpreter.scala
+++ src/compiler/scala/tools/nsc/Interpreter.scala
@@ -91,6 +91,16 @@ class Interpreter(val compiler: Global, output: (String => Unit)) {
   private def reqBinding(vname: Name): Option[Request] = {
     prevRequests.toList.reverse.find(lin => lin.boundNames.contains(vname))
   }
+  
+  /** next line number to use */
+  var nextLineNo = 0
+  
+  /** allocate a fresh line name */
+  def newLineName = {
+    val num = nextLineNo
+    nextLineNo = nextLineNo + 1
+    "line" + num
+  }
  
   /** generate a string using a routine that wants to write on a stream */
   private def stringFrom(writer: PrintWriter=>Unit): String = {
@@ -161,11 +171,10 @@ class Interpreter(val compiler: Global, output: (String => Unit)) {
     // parse
 		val trees = parse(line)
     if(trees.isEmpty) return ()  // parse error or empty input
- 
-    // figure out what kind of request
-    val lineno = prevRequests.length
-    val lineName = "line" + lineno
+   
+    val lineName = newLineName
     
+    // figure out what kind of request
     val req = buildRequest(trees, line, lineName)
     if(req == null) return ()  // a disallowed statement type
       
