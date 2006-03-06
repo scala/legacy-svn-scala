be3ac0a6af5e2ef2651f2cbc491b94bc414c4fe2
michelou <michelou@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2006-03-06 12:45:14 +0000

    removed implicit import of java.io.Console (Java 6), cleaned up code (copyright, tabs, ..)
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@5892 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/tools/nsc/MainInterpreter.scala
+++ src/compiler/scala/tools/nsc/MainInterpreter.scala
@@ -1,11 +1,11 @@
-/* NSC -- new scala compiler
- * Copyright 2005 LAMP/EPFL
+/* NSC -- new Scala compiler
+ * Copyright 2005-2006 LAMP/EPFL
  * @author emir
  */
 // $Id$
 package scala.tools.nsc
 
-import java.io._
+import java.io.{BufferedReader, FileReader, IOException, InputStreamReader}
 import scala.tools.nsc.util.{Position}
 import scala.tools.nsc.reporters.{Reporter, ConsoleReporter}
 
@@ -13,7 +13,7 @@ import scala.tools.nsc.reporters.{Reporter, ConsoleReporter}
  */
 object MainInterpreter {
   val reporter = new ConsoleReporter()
-  
+
   var interpreter: Interpreter = _
 
   /** print a friendly help message */
@@ -25,21 +25,20 @@ object MainInterpreter {
     Console.println("Type :load followed by a filename to load a sequence of interpreter commands.")
     Console.println("Type :help to repeat this message later.")
   }
-  
+
   /** A simple, generic read-eval-print loop with a pluggable eval-print function.
       Loop reading lines of input and invoking the eval-print function.
       Stop looping when eval-print returns false.  */
   def repl(evpr: String => Boolean): Unit = {
     val in = new BufferedReader(new InputStreamReader(System.in))
-    
+
     while(true) {
       Console.print("\nscala> ")
       var line = in.readLine()
-			if(line == null)
+      if(line == null)
         return ()  // assumes null means EOF
- 
+
       val keepGoing = evpr(line)
-      
       if(!keepGoing)
         return ()  // the evpr function said to stop
     }
@@ -49,28 +48,27 @@ object MainInterpreter {
   def interpretOne(line: String): Unit = {
     try {
       interpreter.interpret(line)
-     } catch {
-       case e: Exception => {
-         reporter.info(null,"Exception occurred: " + e.getMessage(),true)
-         //e.printStackTrace()
-       }
-     }
+    } catch {
+      case e: Exception =>
+        reporter.info(null, "Exception occurred: " + e.getMessage(), true)
+        //e.printStackTrace()
+    }
   }
-  
+
   /** interpret all lines from a specified file */
   def interpretAllFrom(filename: String): Unit = {
     val fileIn = try {
       new FileReader(filename)
     } catch {
       case _:IOException =>
-          Console.println("Error opening file: " + filename)
-          null
+        Console.println("Error opening file: " + filename)
+        null
     }
-    if(fileIn == null) return ()
+    if (fileIn == null) return ()
     val in = new BufferedReader(fileIn)
     while(true) {
       val line = in.readLine
-      if(line == null) {
+      if (line == null) {
         fileIn.close
         return ()
       }
@@ -80,17 +78,17 @@ object MainInterpreter {
 
   /** run one command submitted by the user */
   def command(line: String): Boolean = {
-    def withFile(command: String)(action: String=>Unit): Unit = {
+    def withFile(command: String)(action: String => Unit): Unit = {
       val spaceIdx = command.indexOf(' ')
-      if(spaceIdx <= 0) {
+      if (spaceIdx <= 0) {
         Console.println("That command requires a filename to be specified.")
         return ()
       }
       val filename = command.substring(spaceIdx).trim
       action(filename)
     }
-    
-    if(line.startsWith(":"))
+
+    if (line.startsWith(":"))
       line match {
         case ":help" => printHelp
         case ":quit" => return false
@@ -102,31 +100,29 @@ object MainInterpreter {
       interpretOne(line)
     true
   }
-  
- 
-  
+
   /** the main interpreter loop */
   def interpretLoop(compiler: Global): unit = {
-    interpreter = new Interpreter(compiler, str=>Console.print(str))
+    interpreter = new Interpreter(compiler, str => Console.print(str))
     repl(command)
     interpreter.close
   }
 
- 
-
   /** process the command-line arguments and do as they request */
-  def process(args: Array[String]): unit = {
+  def process(args: Array[String]): Unit = {
     val command = new CompilerCommand(List.fromArray(args), error, false)
     reporter.prompt = command.settings.prompt.value
     if (command.settings.help.value) {
-      reporter.info(null, command.usageMsg, true)     
-    } else {
+      reporter.info(null, command.usageMsg, true)
+    }
+    else {
       printHelp
       interpretLoop(new Global(command.settings, reporter))
     }
   }
 
-  def main(args: Array[String]): unit = {
+  def main(args: Array[String]): Unit = {
     process(args)
   }
+
 }
