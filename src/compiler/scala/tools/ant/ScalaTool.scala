f31b87feeaf3aabac58ac0923fbba242e7f4db44
michelou <michelou@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2006-03-28 18:10:56 +0000

    reintroduced attribute 'bootclasspath' in Ant task 'ScalaTool'
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@6962 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/tools/ant/ScalaTool.scala
+++ src/compiler/scala/tools/ant/ScalaTool.scala
@@ -2,18 +2,19 @@
 **   /  |/ / ____/ ____/                                                      **
 **  / | | /___  / /___                                                        **
 ** /_/|__/_____/_____/ Copyright 2005-2006 LAMP/EPFL                          **
-**
-** $Id$
+**                                                                            **
 \*                                                                            */
 
+// $Id$
+
 package scala.tools.ant {
-  
+
   import scala.collection.immutable.{Map, ListMap}
-  
+
   import java.io.{File, InputStream, FileWriter}
   import java.net.{URL, URLClassLoader}
   import java.util.{ArrayList, Vector}
-  
+
   import org.apache.tools.ant.{AntClassLoader, BuildException,
                                DirectoryScanner, Project}
   import org.apache.tools.ant.taskdefs.MatchingTask
@@ -21,7 +22,7 @@ package scala.tools.ant {
   import org.apache.tools.ant.util.{FileUtils, GlobPatternMapper,
                                     SourceFileScanner}
   import org.apache.tools.ant.types.{EnumeratedAttribute, Reference}
-    
+
   /** An Ant task that generates a SH or BAT script to execute a Scala program.
     * This task can take the following parameters as attributes:<ul>
     *  <li>file (mandatory),</li>
@@ -30,22 +31,23 @@ package scala.tools.ant {
     *  <li>platforms,</li>
     *  <li>version,</li>
     *  <li>copyright,</li>
-    *  <li>classpath,</li>
+    *  <li>bootclasspath,</li>
+    *  <li>extclasspath,</li>
     *  <li>properties,</li>
     *  <li>javaflags,</li>
     *  <li>toolflags,</li>
     *  <li>genericfile.</li></ul>
-    * 
+    *
     * @author Gilles Dubochet */
   class ScalaTool extends MatchingTask {
-      
+
     /** The unique Ant file utilities instance to use in this task. */
     private val fileUtils = FileUtils.newFileUtils()
-    
+
 /******************************************************************************\
 **                             Ant user-properties                            **
 \******************************************************************************/
-    
+
     abstract class PermissibleValue {
       val values: List[String]
       def isPermissible(value: String): Boolean =
@@ -56,7 +58,7 @@ package scala.tools.ant {
     object Platforms extends PermissibleValue {
       val values = List("unix", "windows")
     }
-    
+
     /** The path to the exec script file. ".bat" will be appended for the
       * Windows BAT file, if generated. */
     private var file: Option[File] = None
@@ -73,6 +75,8 @@ package scala.tools.ant {
     private var version: String = ""
     /** The optional copyright notice, that will be printed in the script. */
     private var copyright: String = "This file is copyrighted by its owner"
+    /** The optional boot classpath */
+    private var bootclasspath: List[String] = Nil
     /** An (optional) path to all JARs that this script depend on. Paths must be
       * relative to the scala home directory. If not set, all JAR archives in
       * "lib/" are automatically added. */
@@ -88,26 +92,26 @@ package scala.tools.ant {
     /** Additional flags passed to the tool ("java class [toolFlags]"). Can only
       * be set when a main class is defined */
     private var toolFlags: String = ""
-    
+
 /******************************************************************************\
 **                             Properties setters                             **
 \******************************************************************************/
-    
+
     /** Sets the file attribute. Used by Ant.
       * @param input The value of <code>file</code>. */
     def setFile(input: File) =
       file = Some(input)
-      
+
     /** Sets the file attribute. Used by Ant.
       * @param input The value of <code>file</code>. */
     def setName(input: String) =
       name = Some(input)
-      
+
     /** Sets the main class attribute. Used by Ant.
       * @param input The value of <code>mainClass</code>. */
     def setClass(input: String) =
       mainClass = Some(input)
-    
+
     /** Sets the platforms attribute. Used by Ant.
       * @param input The value for <code>platforms</code>. */
     def setPlatforms(input: String) = {
@@ -121,22 +125,27 @@ package scala.tools.ant {
         }
       })
     }
-    
+
     /** Sets the version attribute. Used by Ant.
       * @param input The value of <code>version</code>. */
     def setVersion(input: String) =
       version = input
-    
+
     /** Sets the copyright attribute. Used by Ant.
       * @param input The value of <code>copyright</code>. */
     def setCopyright(input: String) =
       copyright = input
-    
+
+    /** Sets the boot classpath attribute. Used by Ant.
+      * @param input The value of <code>bootclasspath</code>. */
+    def setBootclasspath(input: String) =
+      bootclasspath = bootclasspath ::: List.fromArray(input.split(":"))
+
     /** Sets the extension classpath attribute. Used by Ant.
-      * @param input The value of <code>classpath</code>. */
+      * @param input The value of <code>extclasspath</code>. */
     def setExtclasspath(input: String) =
       extclasspath = extclasspath ::: List.fromArray(input.split(":"))
-    
+
     /** Sets the properties attribute. Used by Ant.
       * @param input The value for <code>properties</code>. */
     def setProperties(input: String) = {
@@ -147,12 +156,12 @@ package scala.tools.ant {
         } else error("Property " + st + " does not conform to specification.")
       })
     }
-    
+
     /** Sets the version attribute. Used by Ant.
       * @param input The value of <code>version</code>. */
     def setJavaflags(input: String) =
       javaFlags = input
-    
+
     /** Sets the version attribute. Used by Ant.
       * @param input The value of <code>version</code>. */
     def setToolflags(input: String) =
@@ -161,42 +170,53 @@ package scala.tools.ant {
 /******************************************************************************\
 **                             Properties getters                             **
 \******************************************************************************/
-    
+
     /** Gets the value of the file attribute in a Scala-friendly form. 
       * @returns The file as a file. */
     private def getFile: File =
       if (file.isEmpty) error("Member 'file' is empty.")
       else getProject().resolveFile(file.get.toString())
 
+    /** Gets the value of the bootclasspath attribute in a Scala-friendly form.
+      * @returns The boot class path as a list of files. */
+    private def getUnixBootClasspath: String =
+      bootclasspath.mkString("", ":", "")
+
+    /** Gets the value of the bootclasspath attribute in a Scala-friendly form.
+      * @returns The boot class path as a list of files. */
+    private def getWinBootClasspath: String =
+      bootclasspath.map(.replace('/', '\\')).
+                mkString("", ";", "")
+
     /** Gets the value of the classpath attribute in a Scala-friendly form.
       * @returns The class path as a list of files. */
     private def getUnixExtClasspath: String =
       extclasspath.mkString("", ":", "")
-    
+
     /** Gets the value of the classpath attribute in a Scala-friendly form.
       * @returns The class path as a list of files. */
     private def getWinExtClasspath: String =
       extclasspath.map(.replace('/', '\\')).
                 mkString("", ";", "")
-      
+
     /** Gets the value of the classpath attribute in a Scala-friendly form.
       * @returns The class path as a list of files. */
     private def getProperties: String =
       properties.map({
         case Pair(name,value) => "-D" + name + "=\"" + value + "\""
       }).mkString("", " ", "")
-    
+
 /******************************************************************************\
 **                       Compilation and support methods                      **
 \******************************************************************************/
-    
+
     /** Generates a build error. Error location will be the current task in the  
       * ant file.
       * @param message A message describing the error.
       * @throws BuildException A build error exception thrown in every case. */
     private def error(message: String): Nothing =
       throw new BuildException(message, getLocation())
-    
+
     private def readResource(resource: String,
                              tokens: Map[String, String]
     ): String = {
@@ -232,7 +252,7 @@ package scala.tools.ant {
       }
       builder.toString()
     }
-    
+
     private def writeFile(file: File, content: String) =
       if (file.exists() && !file.canWrite())
         error("File " + file + " is not writable")
@@ -241,21 +261,22 @@ package scala.tools.ant {
         writer.write(content)
         writer.close()
       }
-    
+
     private def expandUnixVar(vars: Map[String,String]): Map[String,String] =
       vars.map((key:String,vari:String) => vari.
         replaceAll("#([^#]*)#", "\\$$1")
       )
-    
+
     private def expandWinVar(vars: Map[String,String]): Map[String,String] =
       vars.map((key:String,vari:String) => vari.
         replaceAll("#([^#]*)#", "%_$1%")
       )
-    
+
     private def pipeTemplate(template: String, patches: Map[String,String]) = {
       val resourceRoot = "scala/tools/ant/templates/"
       if (platforms.contains("unix")) {
         val unixPatches = expandUnixVar(patches.
+          update("bootclasspath", getUnixBootClasspath).
           update("extclasspath", getUnixExtClasspath))
         val unixTemplateResource = resourceRoot + template + "-unix.tmpl"
         val unixTemplate = readResource(unixTemplateResource, unixPatches)
@@ -263,13 +284,14 @@ package scala.tools.ant {
       }
       if (platforms.contains("windows")) {
         val winPatches = expandWinVar(patches.
+          update("bootclasspath", getWinBootClasspath).
           update("extclasspath", getWinExtClasspath))
         val winTemplateResource = resourceRoot + template + "-windows.tmpl"
         val winTemplate = readResource(winTemplateResource, winPatches)
         writeFile(new File(getFile.getAbsolutePath() + ".bat"), winTemplate)
       }
     }
-      
+
 
 /******************************************************************************\
 **                           The big execute method                           **
@@ -304,7 +326,7 @@ package scala.tools.ant {
         pipeTemplate("tool", patches)
       }
     }
-    
+
   }
-  
+
 }
