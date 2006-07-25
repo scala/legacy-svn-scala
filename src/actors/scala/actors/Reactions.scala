9562403c776f0decbddc6ae06a67d344e458112b
michelou <michelou@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2006-07-25 14:58:41 +0000

    moved trait Reactions to package 'scala.actors'
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@8261 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/actors/scala/actors/Reactions.scala
+++ src/actors/scala/actors/Reactions.scala
@@ -1,7 +1,19 @@
-package scala
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2005-2006, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |                                         **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+package scala.actors
 
 /** An enclosing trait for reactions.
  *  Examples of reactions are: actor.receive's, event.handle's, etc
+ *
+ *  @author Martin Odersky
+ *  @version 1.0
+ *
  *  @param m The input type of a Reaction; typically the type of messages or events.
  */
 trait Reactions[m] {
@@ -20,28 +32,28 @@ trait Reactions[m] {
    *  @param r     The type of values returned by the reaction.
    *               (More precisely, the type of values passed to its continuation)
    *  @param fun   The partial function underlying a reaction
-   */  
+   */
   class Reaction[+r](private val fun: Re[r]) extends Responder[r] {
 
     def respond(k: r => unit): unit = activate(fun)(k)
 
     override def map[s](f: r => s) = new Reaction[s] (
-      fun andThen { 
-        result: ((r => unit) => unit) => 
+      fun andThen {
+        result: ((r => unit) => unit) =>
           k: (s => unit) => result((x: r) => k(f(x)))
       }
     )
 
     def flatMap[s](f: r => Reaction[s]) = new Reaction[s] (
-      fun andThen { 
-        result: ((r => unit) => unit) => 
+      fun andThen {
+        result: ((r => unit) => unit) =>
           k: (s => unit) => result((x: r) => f(x).respond(k))
       }
     )
 
     override def filter(p: r => boolean) = new Reaction[r] (
-      fun andThen { 
-        result: ((r => unit) => unit) => 
+      fun andThen {
+        result: ((r => unit) => unit) =>
           k: (r => unit) => result((x: r) => if (p(x)) k(x) else ())
       }
     )
@@ -50,5 +62,4 @@ trait Reactions[m] {
       this.fun orElse that.fun
     )
   }
-}    
-
+}
