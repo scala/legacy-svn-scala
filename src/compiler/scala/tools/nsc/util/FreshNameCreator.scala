66785308ca6bd05a66c4b198fb69f238409bb09d
michelou <michelou@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2006-09-26 14:16:35 +0000

    removed leading/trailing tabs/blanks in nsc/util/*.scala
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@8770 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/tools/nsc/util/FreshNameCreator.scala
+++ src/compiler/scala/tools/nsc/util/FreshNameCreator.scala
@@ -1,17 +1,18 @@
-/* NSC -- new scala compiler
- * Copyright 2005 LAMP/EPFL
+/* NSC -- new Scala compiler
+ * Copyright 2005-2006 LAMP/EPFL
  * @author  Martin Odersky
  */
 // $Id$
-package scala.tools.nsc.util;
 
-import scala.collection.mutable.HashMap;
+package scala.tools.nsc.util
+
+import scala.collection.mutable.HashMap
 
 class FreshNameCreator {
-    
-  protected var counter = 0;
-  protected val counters = new HashMap[String,int];
-    
+
+  protected var counter = 0
+  protected val counters = new HashMap[String, Int]
+
   /**
    * Create a fresh name with the given prefix. It is guaranteed
    * that the returned name has never been returned by a previous
@@ -22,12 +23,12 @@ class FreshNameCreator {
       case Some(last) => last + 1
       case None => 0
     }
-    counters.update(prefix, count);
+    counters.update(prefix, count)
     prefix + count
   }
 
   def newName(): String = {
-    counter = counter + 1;
+    counter = counter + 1
     "$" + counter + "$"
   }
 }
--- src/compiler/scala/tools/nsc/util/SourceFile.scala
+++ src/compiler/scala/tools/nsc/util/SourceFile.scala
@@ -11,11 +11,12 @@
 package scala.tools.nsc.util
 
 
-import scala.tools.nsc.io.{AbstractFile,VirtualFile}
+import scala.tools.nsc.io.{AbstractFile, VirtualFile}
 
 /** Uses positions that are offsets rather than line/column pairs.
  *
- *  @author Sean McDirmid
+ *  @author  Sean McDirmid
+ *  @version 1.0
  */
 object SourceFile {
   val LF: Char = 0x0A
@@ -58,7 +59,7 @@ class SourceFile(val file: AbstractFile, _content: Array[Char]) {
   // constants
 
   // NOTE: all indexes are based on zero!!!!
-  override def toString(): String = file.name /* + ":" + content.length */ ;
+  override def toString(): String = file.name /* + ":" + content.length */
 
   def dbg(offset: Int) = (new Position(this, offset)).dbgString
 
@@ -66,17 +67,18 @@ class SourceFile(val file: AbstractFile, _content: Array[Char]) {
     var index  = 0
     var offset = 0
 
-    def find(toFind: Int, isIndex: Boolean): Int = { 
+    def find(toFind: Int, isIndex: Boolean): Int = {
       if (toFind == 0) return 0
 
       if (!isIndex) assert(toFind != Position.NOPOS)
       if ( isIndex) assert(toFind > Position.NOLINE - Position.FIRSTLINE)
 
       if (!isIndex && (toFind >= content.length))
-        throw new Error(toFind + " not valid offset in " + file.name + ":" + content.length);
+        throw new Error(toFind + " not valid offset in " +
+                        file.name + ":" + content.length)
 
-      def get(isIndex : Boolean) = if (isIndex) index else offset;
-      
+      def get(isIndex : Boolean) = if (isIndex) index else offset
+
       val isBackward = toFind <= get(isIndex)
       val increment = if (isBackward) -1 else + 1
       val oneIfBackward = if (isBackward) +1 else 0
@@ -84,20 +86,21 @@ class SourceFile(val file: AbstractFile, _content: Array[Char]) {
       // System.err.println("FIND-0: " + toFind + " " + isIndex);
 
       while (true) {
-	// System.err.println("FIND-1: " + offset + " " + index);
+        // System.err.println("FIND-1: " + offset + " " + index);
 
-	if (!isIndex && offset == toFind) return index;
-	if ( isBackward && offset <= 0) throw new Error(offset + " " + index + " " + toFind + " " + isIndex);
-	offset = offset + increment;
-	if (!isBackward) assert(offset < content.length);
+        if (!isIndex && offset == toFind) return index;
+        if (isBackward && offset <= 0)
+          throw new Error(offset + " " + index + " " + toFind + " " + isIndex);
+        offset = offset + increment
+        if (!isBackward) assert(offset < content.length);
 
-	if (isLineBreak(offset + (if (isBackward) 0 else -1))) {
-	  index = index + increment;
-	  if (isIndex && index + oneIfBackward == toFind) 
-	    return      offset + oneIfBackward;
-	}
+        if (isLineBreak(offset + (if (isBackward) 0 else -1))) {
+          index = index + increment
+          if (isIndex && index + oneIfBackward == toFind) 
+            return offset + oneIfBackward;
+        }
       }
-      throw new Error();
+      throw new Error()
     }
   }
   def offsetToLine(offset: Int): Int = line.find(offset, false)
@@ -114,8 +117,9 @@ class SourceFile(val file: AbstractFile, _content: Array[Char]) {
   }
   def path = getFile().path
 
-  def skipWhitespace(offset: Int): Int = 
-    if (Character.isWhitespace(content(offset))) skipWhitespace(offset + 1) else offset;
+  def skipWhitespace(offset: Int): Int =
+    if (Character.isWhitespace(content(offset))) skipWhitespace(offset + 1)
+    else offset
 
   def lineToString(index: Int): String = {
     var offset = lineToOffset(index)
@@ -127,7 +131,7 @@ class SourceFile(val file: AbstractFile, _content: Array[Char]) {
     buf.toString()
   }
 
-  private def normalize(input : Array[char]): Array[char] = 
+  private def normalize(input : Array[char]): Array[char] =
     if (input.length > 0 && input(input.length - 1) == SU) input
     else {
       val content = new Array[char](input.length + 1)
@@ -143,7 +147,7 @@ class CompoundSourceFile(
     components: List[SourceFile],
     contents: Array[Char])
 extends SourceFile(name, contents)
-{   
+{
   /** The usual constructor.  Specify a name for the compound file and
     * a list of component sources */
   def this(name: String, components: SourceFile*) = {
@@ -155,10 +159,11 @@ extends SourceFile(name, contents)
       Array.concat(components.toList.map(comp => 
         comp.content.subArray(0, comp.content.length-1)):_*))
   }
-  
+
   /** Create an instance with the specified components and a generic name. */
-  def this(components: SourceFile*) = this("(virtual file)", components.toList:_*)
-  
+  def this(components: SourceFile*) =
+    this("(virtual file)", components.toList:_*)
+
   override def positionInUltimateSource(position: Position) = {
     var off = position.offset
     var compsLeft = components
@@ -189,7 +194,7 @@ extends SourceFile(name, contents)
       underlyingFile,
       start,
       stop,
-      underlyingFile.content.subArray(start, stop))  
+      underlyingFile.content.subArray(start, stop))
 
   def this(underlyingFile: SourceFile, start: Int, stop: Int) =
     this(
@@ -200,6 +205,6 @@ extends SourceFile(name, contents)
 
   override def positionInUltimateSource(position: Position) = {
     underlyingFile.positionInUltimateSource(
-        new Position(underlyingFile, position.offset + start))
+      new Position(underlyingFile, position.offset + start))
   }
 }
