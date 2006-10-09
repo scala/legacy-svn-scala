55a80c3e19c6fffa18e9296b56e2f35c78b8376b
emir <emir@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2006-10-09 12:58:10 +0000

    updated
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@8895 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- test/pending/pos/bug579.scala
+++ test/pending/pos/bug579.scala
@@ -11,5 +11,11 @@ object Test extends Application {
  
   val x = new MyBean;
   x.frombulizer = "hello"
-   
+
+  x.setFrombulizer ("hola") // synthetic methods comes too late for typechecking this code
+
+  val z:String = x.frombulizer
+
+  val zz:String = x.getFrombulizer  // synthetic methods comes too late for typechecking this code
+
 }
