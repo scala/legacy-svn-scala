884e9d4cfec5b4d2818e27561cae1112588bac05
michelou <michelou@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2006-12-13 09:27:20 +0000

    updated check files with new tuple syntax
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@9510 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- test/files/run/withIndex.scala
+++ test/files/run/withIndex.scala
@@ -4,19 +4,21 @@ object Test {
     val lst: List[String] = List("a", "b", "c")
     val itr: Iterator[String] = lst.elements
     val str: Stream[String] = Stream.fromIterator(lst.elements)
-    
+
     Console.println(ary.zipWithIndex.toList)
     Console.println(lst.zipWithIndex.toList)
     Console.println(itr.zipWithIndex.toList)
     Console.println(str.zipWithIndex.toList)
     assert(ary.zipWithIndex.isInstanceOf[Array[Pair[String,Int]]])
-    
+
     val emptyArray = new Array[String](0)
     val emptyList: List[String] = Nil
     val emptyIterator = emptyList.elements
-  
+    val emptyStream = Stream.empty
+
     Console.println(emptyArray.zipWithIndex.toList)
     Console.println(emptyList.zipWithIndex.toList)
-    Console.println(emptyIterator.zipWithIndex.toList)  
+    Console.println(emptyIterator.zipWithIndex.toList)
+    Console.println(emptyStream.zipWithIndex.toList)
   }
 }
