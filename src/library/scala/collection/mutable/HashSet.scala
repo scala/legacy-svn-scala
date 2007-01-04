ce73b29cb7cc3d017f9a8701064c06430e4d6d19
odersky <odersky@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2007-01-04 16:30:16 +0000

    another change to collection libraries
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@9554 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/library/scala/collection/mutable/HashSet.scala
+++ src/library/scala/collection/mutable/HashSet.scala
@@ -14,15 +14,26 @@ package scala.collection.mutable
 /** This class implements mutable sets using a hashtable.
  *
  *  @author  Matthias Zenger
- *  @version 1.0, 08/07/2003
+ *  @author  Martin Odersky
+ *  @version 2.0, 31/12/2006
  */
+object HashSet {
+
+  /** The empty map of this type */  
+  def empty[A] = new HashSet[A]
+
+  /** The canonical factory for this type
+   */
+  def apply[A](elems: A*) = empty[A] ++ elems
+}
+
 [serializable]
 class HashSet[A] extends Set[A] with HashTable[A] {
 
   def contains(elem: A): Boolean = findEntry(elem) != null
 
   def +=(elem: A) { if (findEntry(elem) == null) addEntry(new SetEntry(elem)) }
-
+ 
   def -=(elem: A) { removeEntry(elem) }
 
   def elements = entries map (.key)
