9996d21924318344cf22811db5ebb2c4a9e47e43
michelou <michelou@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2007-07-09 09:43:09 +0000

    updated svn:keywords, file headers, and scaladoc comments
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@12242 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/library/scala/util/parsing/combinator/lexical/Lexical.scala
+++ src/library/scala/util/parsing/combinator/lexical/Lexical.scala
@@ -1,19 +1,27 @@
 /*                     __                                               *\
 **     ________ ___   / /  ___     Scala API                            **
 **    / __/ __// _ | / /  / _ |    (c) 2006-2007, LAMP/EPFL             **
-**  __\ \/ /__/ __ |/ /__/ __ |                                         **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
 ** /____/\___/_/ |_/____/_/ | |                                         **
 **                          |/                                          **
 \*                                                                      */
+
+// $Id$
+
 
 package scala.util.parsing.combinator.lexical
 
 import scala.util.parsing.syntax._
 import scala.util.parsing.input.CharArrayReader.EofCh
 
-/** This component complements the `Scanners' component with common operations for lexical parsers.
- *<p>
- * {@see StdLexical} for a concrete implementation for a simple, Scala-like language.</p>
+/** <p>
+ *    This component complements the <code>Scanners</code> component with
+ *    common operations for lexical parsers.
+ *  </p>
+ *  <p>
+ *   {@see StdLexical} for a concrete implementation for a simple, Scala-like
+ *   language.
+ *  </p>
  *
  * @author Martin Odersky, Adriaan Moors
  */
@@ -27,7 +35,7 @@ abstract class Lexical extends Scanners with Tokens {
 
   /** A character-parser that matches any character except the ones given in `cs' (and returns it)*/  
   def chrExcept(cs: char*) = elem("", ch => (cs forall (ch !=)))
-  
+
   /** A character-parser that matches a white-space character (and returns it)*/  
   def whitespaceChar = elem("space char", ch => ch <= ' ' && ch != EofCh)
 }
--- src/library/scala/util/parsing/json/JSON.scala
+++ src/library/scala/util/parsing/json/JSON.scala
@@ -1,31 +1,44 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2006-2007, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+// $Id$
+
+
 package scala.util.parsing.json
 
 import scala.collection.mutable.HashMap
 
-/** This object mainly shows how a JSON parser maybe instantiated 
- * 
+/** This object mainly shows how a JSON parser may be instantiated.
+ *
+ *  @author Derek Chen-Becker <java@chen-becker.org>
  */
 object JSON extends Parser {
+
   def parse(input: String) =
     phrase(root)(new lexical.Scanner(input)) match {
       case Success(result, _) => Some(result)
       case _ => None
     }
-  
+
   def parseFull(input: String) = parse(input) match {
     case Some(data) => resolveType(data)
     case None => None
   }
-  
+
   def resolveType(input: Any): Any = 
     input match {
       case jo: List[(String,Any)] => 
         /*println("Resolving object")*/
-        val objMap = new HashMap[String,Any]()
+        val objMap = new HashMap[String, Any]()
         
         jo.foreach { 
-          case (key,value) =>
-            objMap.update(key,resolveType(value))
+          case (key, value) =>
+            objMap.update(key, resolveType(value))
         }
        
           objMap
