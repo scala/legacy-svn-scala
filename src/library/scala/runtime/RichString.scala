e75078260cce6268fbf4f24bdddb5c0a317dcb5d
mcdirmid <mcdirmid@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2007-07-18 07:40:06 +0000

    Updated Stream to be the projection of List. Updated types of ++ to be more consistent. Dealt with overloading ambiguity in ++ as a result of my other changes. Added deprecation comments to Predefs. Added force to projection (returns a copied strict collection).
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@12349 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/library/scala/runtime/RichString.scala
+++ src/library/scala/runtime/RichString.scala
@@ -25,7 +25,12 @@ final class RichString(val self: String) extends Proxy with RandomAccessSeq[Char
                  else if (until > self.length) self.length else until 
     new RichString(self.substring(from0, until0))
   }
-
+  //override def ++ [B >: A](that: Iterable[B]): Seq[B] = {
+  override def ++[B >: Char](that : Iterable[B]) : RandomAccessSeq[B] = that match {
+  case that : RichString => new RichString(self + that.self) 
+  case that => super.++(that)
+  }
+  
   override def take(until : Int) : RichString = slice(0, until)
   override def drop(from : Int)  : RichString = slice(from, self.length)
   override def startsWith[B](that : Seq[B]) = that match {
