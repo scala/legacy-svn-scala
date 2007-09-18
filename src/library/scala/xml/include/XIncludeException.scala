3c241444e3a880aefef0fa98c857d25b1c3ccf03
michelou <michelou@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2007-09-18 09:13:45 +0000

    aded svn:keywords, file headers in XML classes
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@12905 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/library/scala/xml/include/XIncludeException.scala
+++ src/library/scala/xml/include/XIncludeException.scala
@@ -1,3 +1,13 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2007, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+// $Id$
+
 package scala.xml.include
 
 /**
@@ -16,13 +26,13 @@ package scala.xml.include
  * @param   message   the detail message.
  */
 class XIncludeException(message: String) extends Exception(message) {
-  
+
   /**
    * uses <code>null</code> as its error detail message.
    */
-  def this() = this(null);
-  
-  private var rootCause: Throwable = null;
+  def this() = this(null)
+
+  private var rootCause: Throwable = null
 
   /**
    * When an <code>IOException</code>, <code>MalformedURLException</code> 
@@ -34,10 +44,10 @@ class XIncludeException(message: String) extends Exception(message) {
    * @param   nestedException   the underlying exception which 
    caused the XIncludeException to be thrown
    */
-  def setRootCause(nestedException: Throwable ): Unit = {
-    this.rootCause = nestedException;     
+  def setRootCause(nestedException: Throwable ) {
+    this.rootCause = nestedException
   }
-  
+
   /**
    * When an <code>IOException</code>, <code>MalformedURLException</code> 
    * or other generic exception is thrown while processing an XML document
@@ -46,11 +56,9 @@ class XIncludeException(message: String) extends Exception(message) {
    * This method allows you to retrieve the original exception.
    * It returns null if no such exception caused this <code>XIncludeException</code>.
    *
-   * @return   Throwable   the underlying exception which 
-   caused the XIncludeException to be thrown
+   * @return Throwable   the underlying exception which caused the
+   *                     <code>XIncludeException</code> to be thrown
    */
-  def getRootCause(): Throwable = {
-    return this.rootCause;     
-  }
+  def getRootCause(): Throwable = this.rootCause
   
 }
