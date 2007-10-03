c9016998fc1de303072395f7a9d0479ca410dcb9
michelou <michelou@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2007-10-03 16:52:05 +0000

    enums can't be serialized
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@12990 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- test/files/jvm/serialization.scala
+++ test/files/jvm/serialization.scala
@@ -49,7 +49,7 @@ object Test1_scala {
   val x2 = None
   val x3 = Array(1, 2, 3)
   val x4 = { x: Int => 2 * x }
- 
+
   try {
     val y1: List[Nothing]      = Serialize.read(Serialize.write(x1))
     val y2: Option[Nothing]    = Serialize.read(Serialize.write(x2))
@@ -59,19 +59,19 @@ object Test1_scala {
     println("x1 = " + x1)
     println("y1 = " + y1)
     println("x1 eq y1: " + (x1 eq y1) + " - y1 eq x1: " + (y1 eq x1))
-    println()
+    println
     println("x2 = " + x2)
     println("y2 = " + y2)
     println("x2 eq y2: " + (x2 eq y2) + " - y2 eq x2: " + (y2 eq x2))
-    println()
+    println
     println("x3 = " + arrayToString(x3))
     println("y3 = " + arrayToString(y3))
     println("arrayEquals(x3, y3): " + arrayEquals(x3, y3))
-    println()
+    println
     println("x4 = <na>")
     println("y4 = <na>")
     println("x4(2): " + x4(2) + " - y4(2): " + y4(2))
-    println()
+    println
   }
   catch {
     case e: Exception =>
