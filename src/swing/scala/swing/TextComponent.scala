1dee611ddc98fedd8fe5dca7224b3add87ab6b66
maier <maier@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2008-04-18 14:25:30 +0000

    scala.swing update
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@14701 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/swing/scala/swing/TextComponent.scala
+++ src/swing/scala/swing/TextComponent.scala
@@ -5,15 +5,30 @@ import javax.swing.text._
 import javax.swing.event._
 import event._
 
+object TextComponent {
+  trait HasColumns extends TextComponent { 
+    def columns: Int
+    def columns_=(n: Int)
+  }
+  trait HasRows extends TextComponent {
+    def rows: Int
+    def rows_=(n: Int)
+  }
+}
+
 class TextComponent(override val peer: JTextComponent) extends Component with EditorComponent with Publisher {
-  def text: String = peer.getText
-  def text_=(x: String) = peer.setText(x)
-
-  val caret = new Caret(peer.getCaret)
+  def contents: String = peer.getText
+  def contents_=(t: String) = peer.setText(t)
   
-  def content: String = peer.getText
-  def content_=(v: String) { peer.setText(v) }
+  val caret = new Caret(peer.getCaret)
 
+  def editable: Boolean = peer.isEditable
+  def editable_=(x: Boolean) = peer.setEditable(x)
+  
+  def cut() { peer.cut() }
+  def copy() { peer.copy() }
+  def selected: String = peer.getSelectedText
+  
   peer.addCaretListener {
     new CaretListener {
       def caretUpdate(e: CaretEvent) { publish(CaretUpdate(TextComponent.this)) } 
