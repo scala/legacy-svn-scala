c78dc07b8ae9dca4a44e940229c03503ab3439be
michelou <michelou@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2008-06-10 08:46:06 +0000

    int -> Int, etc..
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@15311 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- test/files/shootout/takfp.scala
+++ test/files/shootout/takfp.scala
@@ -7,14 +7,14 @@
 
 object takfp
 {
-  def main(args: Array[String]): unit =
+  def main(args: Array[String])
   {
     var n = Integer.parseInt(args(0));
 
     System.out.println(tak(n * 3.0f, n * 2.0f, n * 1.0f));
   }
 
-  final def tak(x: float, y: float, z: float): float =
+  final def tak(x: Float, y: Float, z: Float): Float =
   {
     if (y >= x) return z;
     return tak(tak(x - 1.0f, y, z), tak(y - 1.0f, z, x), tak(z - 1.0f, x, y));
