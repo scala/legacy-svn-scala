4c635157909a3717b2141c9f841612c50ea24356
stepancheg <stepancheg@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2008-06-27 09:22:09 +0000

    Add RandomAccessSeqProxy
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@15448 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/library/scala/collection/mutable/PriorityQueueProxy.scala
+++ src/library/scala/collection/mutable/PriorityQueueProxy.scala
@@ -20,7 +20,7 @@ package scala.collection.mutable
  *  @version 1.0, 03/05/2004
  */
 abstract class PriorityQueueProxy[A <% Ordered[A]] extends PriorityQueue[A]
-         with SeqProxy[A]
+         with RandomAccessSeqProxy[A]
 {
 
   def self: PriorityQueue[A]
@@ -90,10 +90,6 @@ abstract class PriorityQueueProxy[A <% Ordered[A]] extends PriorityQueue[A]
    */
   override def toQueue: Queue[A] = self.toQueue
 
-  override def take(until : Int) = self take until
-  override def drop(from : Int) = self drop from
-  override def slice(from : Int, until : Int) = self.slice(from, until)
-  
   /** This method clones the priority queue.
    *
    *  @return  a priority queue with the same elements.
