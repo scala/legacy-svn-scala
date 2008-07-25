bbbf2e508fc7eda656a941e4889c237cd3ba8171
mcdirmid <mcdirmid@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2008-07-25 12:27:02 +0000

    Hardening of plugin to convert more hard crashes as soft ones.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@15610 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/tools/nsc/typechecker/Contexts.scala
+++ src/compiler/scala/tools/nsc/typechecker/Contexts.scala
@@ -13,7 +13,7 @@ import scala.collection.mutable.ListBuffer
 /** This trait ...
  *
  *  @author  Martin Odersky
- *  @version 1.0
+ *  @version 1.0 
  */
 trait Contexts { self: Analyzer =>
   import global._
@@ -337,14 +337,14 @@ trait Contexts { self: Analyzer =>
 
     def error(pos: Position, err: Error) {
       val msg = err.getMessage()
-      if (reportGeneralErrors)
+      if (reportGeneralErrors || inIDE)
         unit.error(pos, if (checking) "**** ERROR DURING INTERNAL CHECKING ****\n" + msg else msg)
       else
         throw err
     }
 
     def error(pos: Position, msg: String) {
-      if (reportGeneralErrors)
+      if (reportGeneralErrors || inIDE)
         unit.error(pos, if (checking) "**** ERROR DURING INTERNAL CHECKING ****\n" + msg else msg)
       else
         throw new TypeError(pos, msg)
