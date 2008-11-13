3459dae1d9561bcfa93a7d9051ac8a98bf2c1b14
DRMacIver <DRMacIver@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2008-11-13 22:14:16 +0000

    Starting on improving the abstraction level of the pattern matcher (code from paul)
    Still needs a fair bit of work.
    
    
    
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@16553 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/tools/nsc/matching/CodeFactory.scala
+++ src/compiler/scala/tools/nsc/matching/CodeFactory.scala
@@ -43,7 +43,7 @@ trait CodeFactory {
         case Literal(Constant(value)) => Some(value)
         case _ => None
       }
-    }
+    }    
   }
 
   final def typedValDef(x: Symbol, rhs: Tree)(implicit typer: Typer) = x.tpe match {
@@ -96,12 +96,8 @@ trait CodeFactory {
   final def Equals  (left: Tree, right: Tree): Tree = fn(left, nme.EQ, right)
   final def Eq      (left: Tree, right: Tree): Tree = fn(left, nme.eq, right)
   final def GTE     (left: Tree, right: Tree): Tree = fn(left, nme.GE, right) // >=
-
-  final def Not(arg: Tree) = 
-    Select(arg, NOT)
-  
-  final def And(left: Tree, right: Tree): Tree = 
-    fn(left, AND, right)
+  final def And     (left: Tree, right: Tree): Tree = fn(left, AND, right)
+  final def Not                   (arg: Tree): Tree = Select(arg, NOT)
 
   final def ThrowMatchError(pos: Position, obj: Tree) = atPos(pos) {
     Throw( New(TypeTree(MatchErrorClass.tpe), List(List(obj))) )
