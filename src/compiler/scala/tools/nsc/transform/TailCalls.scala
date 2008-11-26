a448b61cbc255b495dec374c9f0da111c30e542c
dragos <dragos@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2008-11-26 18:58:03 +0000

    Fixed 1541.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@16654 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/tools/nsc/transform/TailCalls.scala
+++ src/compiler/scala/tools/nsc/transform/TailCalls.scala
@@ -252,9 +252,13 @@ abstract class TailCalls extends Transform
                isSameTypes(ctx.tparams, targs map (_.tpe.typeSymbol)) &&
                isRecursiveCall(fun)) {
             fun match {
-              case Select(receiver, _) => 
-                // make sure the type of 'this' doesn't change through this recursive call
-                if (!forMSIL && (receiver.tpe.widen == ctx.currentMethod.enclClass.typeOfThis)) 
+              case Select(receiver, _) =>
+                val recTpe = receiver.tpe.widen
+                val enclTpe = ctx.currentMethod.enclClass.typeOfThis
+                // make sure the type of 'this' doesn't change through this polymorphic recursive call
+                if (!forMSIL && 
+                    (receiver.tpe.typeParams.isEmpty || 
+                      (receiver.tpe.widen == ctx.currentMethod.enclClass.typeOfThis))) 
                   rewriteTailCall(fun, receiver :: transformTrees(vargs, mkContext(ctx, false))) 
                 else 
                   defaultTree
