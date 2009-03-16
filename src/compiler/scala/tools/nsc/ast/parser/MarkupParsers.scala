bc69bd3536b40d3886fc27374ef177812838b271
extempore <extempore@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-03-16 17:46:58 +0000

    The birth of the @switch and @tailrec annotations. closing bug #709. They are located in package scala.annotation.  Also in this patch:  * numerous test cases for both annotations  * addition of @tailrec and @switch in a few strategic locations  * fixes for critical section NewScanners methods which were not being    compiled into switches, immediately proving the value of @switch  * tail recursive implementations for Iterator.{ dropWhile, drop}    and List.dropWhile tagged with @tailrec, closing bug #1376
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@17315 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/tools/nsc/ast/parser/MarkupParsers.scala
+++ src/compiler/scala/tools/nsc/ast/parser/MarkupParsers.scala
@@ -10,6 +10,7 @@ import scala.collection.mutable
 import scala.tools.nsc.util.{Position,NoPosition,SourceFile}
 import scala.xml.{Text, TextBuffer}
 import SourceFile.{SU,LF}
+import scala.annotation.switch
 
 /** This trait ...
  *
@@ -237,8 +238,8 @@ trait MarkupParsers {self: Parsers =>
       val hex = (ch == 'x') && { nextch; true }
       val base = if (hex) 16 else 10
       var i = 0
-      while (ch != ';') {
-        ch match {
+      while (ch != ';') { 
+        (ch: @switch) match {
           case '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' =>
             i = i * base + ch.asDigit
           case 'a' | 'b' | 'c' | 'd' | 'e' | 'f'
