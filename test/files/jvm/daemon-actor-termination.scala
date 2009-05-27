75656794d167ffe0efb7b76d332d8fb0fbbb5ba4
phaller <phaller@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-05-27 22:58:02 +0000

    Added test for #2000. Added tests for looping and normal exit.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@17861 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- test/files/jvm/daemon-actor-termination.scala
+++ test/files/jvm/daemon-actor-termination.scala
@@ -1,19 +1,27 @@
-import scala.actors.DaemonActor 
+import scala.actors.{Actor, DefaultExecutorScheduler}
 
-/* Test that a DaemonActor that hasn't finished does not prevent termination */ 
- 
-object Test { 
-  class MyDaemon extends DaemonActor { 
-    def act() { 
-      println("I'm going to make you wait.") 
-      Thread.sleep(5000) 
-      println("Ok, I'm done.") 
-    } 
-  } 
-  def main(args: Array[String]) { 
-    val daemon = new MyDaemon 
-    daemon.start() 
-    Thread.sleep(500) // give the daemon a chance to start 
-    println("I'm tired of waiting for you.  Good bye.") 
-  } 
+/* Test that a daemon Actor that hasn't finished does not prevent termination */
+
+trait DaemonActor extends Actor {
+  override def scheduler =
+    Test.daemonSched
+}
+
+object Test {
+  val daemonSched =
+    new DefaultExecutorScheduler(true)
+
+  class MyDaemon extends DaemonActor {
+    def act() {
+      println("I'm going to make you wait.")
+      Thread.sleep(5000)
+      println("Ok, I'm done.")
+    }
+  }
+  def main(args: Array[String]) {
+    val daemon = new MyDaemon
+    daemon.start()
+    Thread.sleep(500) // give the daemon a chance to start
+    println("I'm tired of waiting for you.  Good bye.")
+  }
 }
