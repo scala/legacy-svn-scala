075214bef4473933c8c9c6b57bafeab2c9020347
odersky <odersky@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-08-27 14:45:35 +0000

    added manifests to most parts of standard library which deal with arrays. One test is temporarily disabled, as it shows a deep problem with multi-dimensional arrays (which was present all along).
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@18589 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/library/scala/collection/generic/VectorTemplate.scala
+++ src/library/scala/collection/generic/VectorTemplate.scala
@@ -171,6 +171,7 @@ trait VectorTemplate[+A, +This <: VectorTemplate[A, This] with Vector[A]] extend
     }
   }
 
+
   // Overridden methods from Sequence
   
   override def lengthCompare(len: Int): Int = length - len
@@ -259,6 +260,7 @@ trait VectorTemplate[+A, +This <: VectorTemplate[A, This] with Vector[A]] extend
       super.endsWith(that)
   }
 
+  
   override def equals(that: Any): Boolean = that match {
     case that: Vector[_]  => this.length == that.length && startsWith(that, 0)
     case _                => super.equals(that)
