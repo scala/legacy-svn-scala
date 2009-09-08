7f5bb9506d2640bc38b00744ea391ebf4610c847
michelou <michelou@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-09-08 16:24:42 +0000

    added a few @serializable annotations, added @SerialVersionUID to anonfuns, several cleanups
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@18660 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/library/scala/collection/immutable/Vector.scala
+++ src/library/scala/collection/immutable/Vector.scala
@@ -17,14 +17,14 @@ trait Vector[+A] extends Sequence[A]
                     with collection.Vector[A] 
                     with TraversableClass[A, Vector]
                     with VectorTemplate[A, Vector[A]] { 
-  override def companion: Companion[Vector]  = Vector
+  override def companion: Companion[Vector] = Vector
 }
 
 object Vector extends SequenceFactory[Vector] {
   class Impl[A](buf: ArrayBuffer[A]) extends Vector[A] { // todo: insert better vector implementation here
     def length = buf.length
     def apply(idx: Int) = buf.apply(idx)
-  } 
+  }
   implicit def builderFactory[A]: BuilderFactory[A, Vector[A], Coll] = new VirtualBuilderFactory[A]
   def newBuilder[A]: Builder[A, Vector[A]] = new ArrayBuffer[A] mapResult (buf => new Impl(buf))
 }
