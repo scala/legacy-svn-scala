22cc6dc998e48591ce81b04e7478b9749154114e
extempore <extempore@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-11-21 23:55:06 +0000

    Cleanup of Cleanup.  Finally straightening out a bunch of duplicated boxing code in the right location.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@19764 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/tools/nsc/ast/TreeDSL.scala
+++ src/compiler/scala/tools/nsc/ast/TreeDSL.scala
@@ -198,6 +198,10 @@ trait TreeDSL {
     def AND(guards: Tree*) =
       if (guards.isEmpty) EmptyTree
       else guards reduceLeft gen.mkAnd
+      
+    def OR(guards: Tree*) =
+      if (guards.isEmpty) EmptyTree
+      else guards reduceLeft gen.mkOr      
     
     def IF(tree: Tree)    = new IfStart(tree, EmptyTree)
     def TRY(tree: Tree)   = new TryStart(tree, Nil, EmptyTree)
