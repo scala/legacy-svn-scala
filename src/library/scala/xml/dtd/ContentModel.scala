b390458e15a6db8592abfbaff8cf4aef05dac99e
extempore <extempore@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-11-24 00:08:32 +0000

    Some more XML cleanups.  I'm seeing if I can break the compiler dependency on scala.util.automata.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@19803 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/library/scala/xml/dtd/ContentModel.scala
+++ src/library/scala/xml/dtd/ContentModel.scala
@@ -50,12 +50,10 @@ object ContentModel extends WordExp {
   def buildString(r: RegExp): String = sbToString(buildString(r, _))
 
   /* precond: rs.length >= 1 */
-  private def buildString(rs: Seq[RegExp], sb: StringBuilder, sep: Char) {    
-    val it = rs.iterator
-    val fst = it.next
-    buildString(fst, sb)
-    for (z <- it) {
-      sb.append(sep)
+  private def buildString(rs: Seq[RegExp], sb: StringBuilder, sep: Char) {
+    buildString(rs.head, sb)
+    for (z <- rs.tail) {
+      sb append sep
       buildString(z, sb)
     }
     sb
@@ -121,7 +119,7 @@ case class MIXED(r: ContentModel.RegExp) extends DFAContentModel {
   }
 }
 
-case class  ELEMENTS(r: ContentModel.RegExp) extends DFAContentModel {
+case class ELEMENTS(r: ContentModel.RegExp) extends DFAContentModel {
   override def buildString(sb: StringBuilder): StringBuilder =  
     ContentModel.buildString(r, sb)
 }
