03b3d75d0990bd8aa0242fcb13e60ead309bbaf9
moors <moors@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2009-11-25 15:48:19 +0000

    quick fix for #2673: relaxed zipped on Tuple2/Tuple3 (back to using A => B for constraint instead of A <%< B)
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@19861 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/library/scala/Tuple2.scala
+++ src/library/scala/Tuple2.scala
@@ -12,7 +12,7 @@
 
 package scala
 
-import scala.collection.{ TraversableLike, IterableLike }
+import scala.collection.{TraversableLike, IterableLike}
 import scala.collection.generic.CanBuildFrom
 
 
@@ -29,38 +29,36 @@ case class Tuple2[+T1, +T2](_1:T1, _2:T2) extends Product2[T1, T2]  {
   /** Swap the elements of the tuple */
   def swap: Tuple2[T2,T1] = Tuple2(_2, _1)
 
-// TODO: probably loosen zip and zipped from <:< to <%<
-
-  def zip[Repr1, El1, El2, To](implicit w1:   T1 <:< TraversableLike[El1, Repr1], 
-                                        w2:   T2 <:< Iterable[El2], 
+  def zip[Repr1, El1, El2, To](implicit w1:   T1 => TraversableLike[El1, Repr1],
+                                        w2:   T2 => Iterable[El2],
                                         cbf1: CanBuildFrom[Repr1, (El1, El2), To]): To = {
     val coll1: TraversableLike[El1, Repr1] = _1
     val coll2: Iterable[El2] = _2
     val b1 = cbf1(coll1.repr)
     val elems2 = coll2.iterator
-   
+
     for(el1 <- coll1)
       if(elems2.hasNext)
         b1 += ((el1, elems2.next))
-     
+
     b1.result
   }
-    
-	def zipped[Repr1, El1, Repr2, El2](implicit w1: T1 <:< TraversableLike[El1, Repr1], w2: T2 <:< IterableLike[El2, Repr2]): Zipped[Repr1, El1, Repr2, El2]
-		= new Zipped[Repr1, El1, Repr2, El2](_1, _2)
-		  
+
+  def zipped[Repr1, El1, Repr2, El2](implicit w1: T1 => TraversableLike[El1, Repr1], w2: T2 => IterableLike[El2, Repr2]): Zipped[Repr1, El1, Repr2, El2]
+    = new Zipped[Repr1, El1, Repr2, El2](_1, _2)
+
   class Zipped[+Repr1, +El1, +Repr2, +El2](coll1: TraversableLike[El1, Repr1], coll2: IterableLike[El2, Repr2]) { // coll2: IterableLike for filter
     def map[B, To](f: (El1, El2) => B)(implicit cbf: CanBuildFrom[Repr1, B, To]): To = {
-     val b = cbf(coll1.repr)
-     val elems2 = coll2.iterator
-   
-     for(el1 <- coll1)
+      val b = cbf(coll1.repr)
+      val elems2 = coll2.iterator
+
+      for(el1 <- coll1)
        if(elems2.hasNext)
          b += f(el1, elems2.next)
-   
-     b.result
+
+      b.result
     }
-  
+
     def flatMap[B, To](f: (El1, El2) => Traversable[B])(implicit cbf: CanBuildFrom[Repr1, B, To]): To = {
       val b = cbf(coll1.repr)
       val elems2 = coll2.iterator
@@ -71,12 +69,12 @@ case class Tuple2[+T1, +T2](_1:T1, _2:T2) extends Product2[T1, T2]  {
 
       b.result
     }
-    
+
     def filter[To1, To2](f: (El1, El2) => Boolean)(implicit cbf1: CanBuildFrom[Repr1, El1, To1], cbf2: CanBuildFrom[Repr2, El2, To2]): (To1, To2) = {
       val b1 = cbf1(coll1.repr)
       val b2 = cbf2(coll2.repr)
       val elems2 = coll2.iterator
-     
+
       for(el1 <- coll1) {
         if(elems2.hasNext) {
           val el2 = elems2.next
@@ -86,10 +84,10 @@ case class Tuple2[+T1, +T2](_1:T1, _2:T2) extends Product2[T1, T2]  {
           }
         }
       }
-       
+
       (b1.result, b2.result)
-    }  
-  
+    }
+
     def exists(f: (El1, El2) => Boolean): Boolean = {
       var acc = false
       val elems2 = coll2.iterator
@@ -100,7 +98,7 @@ case class Tuple2[+T1, +T2](_1:T1, _2:T2) extends Product2[T1, T2]  {
 
       acc
     }
-  
+
     def forall(f: (El1, El2) => Boolean): Boolean = {
       var acc = true
       val elems2 = coll2.iterator
@@ -111,7 +109,7 @@ case class Tuple2[+T1, +T2](_1:T1, _2:T2) extends Product2[T1, T2]  {
 
       acc
     }
-  
+
     def foreach[U](f: (El1, El2) => U): Unit = {
       val elems2 = coll2.iterator
 
