b4ac99e9413637503bc8f8e044d8e0152312a8b8
extempore <extempore@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2010-02-23 06:57:53 +0000

    Abstracting out a few more platform specific elements now that we have a facility for doing so.  Review by rytz.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@20956 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/tools/nsc/util/MsilClassPath.scala
+++ src/compiler/scala/tools/nsc/util/MsilClassPath.scala
@@ -17,7 +17,7 @@ import scala.collection.mutable.{ ListBuffer, HashSet => MutHashSet }
 import scala.tools.nsc.io.AbstractFile
 
 import ch.epfl.lamp.compiler.msil.{ Type => MSILType, Assembly }
-import ClassPath.ClassPathContext
+import ClassPath.{ ClassPathContext, isTraitImplementation }
 
 /** Keeping the MSIL classpath code in its own file is important to make sure
  *  we don't accidentally introduce a dependency on msil.jar in the jvm.
@@ -34,6 +34,18 @@ object MsilClassPath {
     }
     res
   }
+  
+  /** On the java side this logic is in PathResolver, but as I'm not really
+   *  up to folding MSIL into that, I am encapsulating it here.
+   */
+  def fromSettings(settings: Settings): MsilClassPath = {
+    val context =
+      if (settings.inline.value) new MsilContext
+      else new MsilContext { override def isValidName(name: String) = !isTraitImplementation(name) }
+    
+    import settings._
+    new MsilClassPath(assemextdirs.value, assemrefs.value, sourcepath.value, context)
+  }  
   
   class MsilContext extends ClassPathContext[MSILType] {
     def toBinaryName(rep: MSILType) = rep.Name
