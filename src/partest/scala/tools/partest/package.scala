1fa594d7cfb3bc04a6687f008ae184caa9367669
extempore <extempore@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2010-03-02 20:28:45 +0000

    Removed the symlinks between scalacheck jars to satisfy windows. Tweaked partest to accomodate.  No review.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@21044 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/partest/scala/tools/partest/package.scala
+++ src/partest/scala/tools/partest/package.scala
@@ -10,35 +10,11 @@ import nsc.Properties.{ propOrElse, propOrNone, propOrEmpty }
 
 package object partest { 
   import nest.NestUI
-
+  
   def basename(name: String): String = Path(name).stripExtension
   def resultsToStatistics(results: Iterable[(_, Int)]): (Int, Int) = {
     val (files, failures) = results map (_._2 == 0) partition (_ == true)
     (files.size, failures.size)
-  }
-  
-  object PartestDefaults {
-    import nsc.Properties._
-    private def wrapAccessControl[T](body: => Option[T]): Option[T] =
-      try body catch { case _: java.security.AccessControlException => None }
-    
-    def testRootName  = propOrNone("partest.root")
-    def srcDirName    = propOrElse("partest.srcdir", "files")
-    def testRootDir   = testRootName map (x => Directory(x))
-
-    def classPath   = PathResolver.Environment.javaUserClassPath    // XXX
-
-    def javaCmd     = propOrElse("partest.javacmd", "java")
-    def javacCmd    = propOrElse("partest.javac_cmd", "javac")
-    def javaOpts    = propOrElse("partest.java_opts", "")
-    def scalacOpts  = propOrElse("partest.scalac_opts", "-deprecation")
-
-    def testBuild   = propOrNone("partest.build")
-    def errorCount  = propOrElse("partest.errors", "0").toInt
-    def numActors   = propOrElse("partest.actors", "8").toInt
-    def poolSize    = wrapAccessControl(propOrNone("actors.corePoolSize"))
-
-    def timeout     = "1200000"
   }
   
   def vmArgString = {    
