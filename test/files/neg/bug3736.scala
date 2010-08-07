9e53d3fd3959954509180c23091a1b97bc003192
extempore <extempore@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2010-08-07 21:06:46 +0000

    Modified r22702 to avoid tarring overloads with the same brush. No review.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@22703 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- test/files/neg/bug3736.scala
+++ test/files/neg/bug3736.scala
@@ -7,6 +7,19 @@ object Test {
     def f5 = super.!=(new AnyRef)
     def f6 = super.##
   }
+  
+  // Ill-advised overloads to be sure...
+  class B {
+    def ##(x: String) = true
+    def ==(x1: String, xs: List[_]) = true
+    def !=(x1: String, xs: List[_]) = true
+  }
+  
+  class C extends B {
+    override def ##(x: String) = super.##(x)
+    override def ==(x1: String, xs: List[_]) = super.==(x1, xs)
+    override def !=(x1: String, xs: List[_]) = super.!=(x1, xs)
+  }
 
   def main(args: Array[String]): Unit = {
     val x = new A
