9b8c6c2b1622303bf16dd2254d45c1a672e93ef4
extempore <extempore@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2010-11-01 20:31:12 +0000

    Created @strictfp annotation in scala.annotation.  If you prefer your fp with no tolerance for tomfoolery, this is the commit for you. Closes #1708, review by community.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@23427 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/tools/nsc/backend/jvm/GenJVM.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/GenJVM.scala
@@ -634,6 +634,9 @@ abstract class GenJVM extends SubComponent {
       var flags = javaFlags(m.symbol)
       if (jclass.isInterface())
         flags |= ACC_ABSTRACT
+        
+      if (m.symbol.isStrictFP)
+        flags |= ACC_STRICT
       
       // native methods of objects are generated in mirror classes
       if (method.native)
