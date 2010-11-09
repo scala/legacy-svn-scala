5ad1a6f11315b5fdacbdbc020c8ffb9d6ecbe9d1
extempore <extempore@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2010-11-09 03:13:17 +0000

    While investigating the cause of #3984, discovered that set hashcodes were being calculated like:
      elems map (_.hashCode) sum
    
    This looks totally correct, except that because of set+map
    semantics it drops all the duplicate hashcodes and therefore
    calculates the wrong sum anytime there are unequal elements
    with equal hashcodes.  If we're married to the set+map semantics
    (and I don't know what could be done about it) I had better
    get a proper -Xlint going because this is one of those traps
    which shoots bees out of its mouth.  No review.
    
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@23475 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/library/scala/collection/mutable/SetLike.scala
+++ src/library/scala/collection/mutable/SetLike.scala
@@ -67,9 +67,6 @@ trait SetLike[A, +This <: SetLike[A, This] with Set[A]]
    *  for better efficiency.
    */
   override protected[this] def newBuilder: Builder[A, This] = empty
-  
-  @migration(2, 8, "Set.map now returns a Set, so it will discard duplicate values.")
-  override def map[B, That](f: A => B)(implicit bf: CanBuildFrom[This, B, That]): That = super.map(f)(bf)
 
   /** Adds an element to this $coll.
    *
