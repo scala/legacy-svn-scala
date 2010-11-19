9a0f40fc41e21a0ea2d038297055596ef18ad69b
prokopec <prokopec@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2010-11-19 12:38:14 +0000

    Fixes #3686 by special casing Stream transformers depending on target type.
    Review by extempore.
    
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@23552 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/library/scala/collection/package.scala
+++ src/library/scala/collection/package.scala
@@ -69,7 +69,7 @@ package scala
  */
 package object collection {
   import scala.collection.generic.CanBuildFrom // can't refer to CanBuild here
-
+  
   /** Provides a CanBuildFrom instance that builds a specific target collection (`To') irrespective of the original collection (`From').
    */
   def breakOut[From, T, To](implicit b : CanBuildFrom[Nothing, T, To]) =
