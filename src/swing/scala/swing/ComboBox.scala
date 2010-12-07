56c0ae61b463eca94b82165a7d2e18cf776996c8
Paul Phillips <paulp@improving.org>
2010-12-07 11:44:22 -0800

    Inserted null to make it buildable.

--- src/swing/scala/swing/ComboBox.scala
+++ src/swing/scala/swing/ComboBox.scala
@@ -120,7 +120,8 @@ object ComboBox {
   implicit def floatEditor(c: ComboBox[Float]): Editor[Float] = new BuiltInEditor(c)(s => s.toFloat, s => s.toString)
   implicit def doubleEditor(c: ComboBox[Double]): Editor[Double] = new BuiltInEditor(c)(s => s.toDouble, s => s.toString)
   
-  def newConstantModel[A](items: Seq[A]): ComboBoxModel = {
+  // XXX Returning null since there's not yet a workaround for #3634.
+  def newConstantModel[A](items: Seq[A]): ComboBoxModel = { null
     // [scalacfork]  anonymous class $anon inherits different type instances of trait ListModel:
     // [scalacfork] javax.swing.ListModel and javax.swing.ListModel[AnyRef]
     // [scalacfork]     new AbstractListModel[AnyRef] with ComboBoxModel {
@@ -131,19 +132,19 @@ object ComboBox {
     // [scalacfork]     new AbstractListModel with ComboBoxModel {
     // [scalacfork]         ^
     
-    new AbstractListModel[A] with ComboBoxModel {
-      private var selected: A = if (items.isEmpty) null.asInstanceOf[A] else items(0)
-      def getSelectedItem = selected.asInstanceOf[AnyRef]
-      def setSelectedItem(a: Any) { 
-        if ((selected != null && selected != a) ||
-            selected == null && a != null) {
-          selected = a.asInstanceOf[A]
-          fireContentsChanged(this, -1, -1)
-        }
-      } 
-      def getElementAt(n: Int) = items(n).asInstanceOf[A]
-      def getSize = items.size
-    }
+    // new AbstractListModel[A] with ComboBoxModel {
+    //   private var selected: A = if (items.isEmpty) null.asInstanceOf[A] else items(0)
+    //   def getSelectedItem = selected.asInstanceOf[AnyRef]
+    //   def setSelectedItem(a: Any) { 
+    //     if ((selected != null && selected != a) ||
+    //         selected == null && a != null) {
+    //       selected = a.asInstanceOf[A]
+    //       fireContentsChanged(this, -1, -1)
+    //     }
+    //   } 
+    //   def getElementAt(n: Int) = items(n).asInstanceOf[A]
+    //   def getSize = items.size
+    // }
   }
   
   /*def newMutableModel[A, Self](items: Seq[A] with scala.collection.mutable.Publisher[scala.collection.mutable.Message[A], Self]): ComboBoxModel = {
