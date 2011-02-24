778fa595592796882f4c3cb13058e9bf4555501e
plocinic <plocinic@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2011-02-24 10:41:37 +0000

    lazy val calls shouldn't be included in pure expressions in general. closes 4047. fixed also bug in optimizer (was eliminating a.foo call in the test). review by dragos
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@24345 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/tools/nsc/transform/Erasure.scala
+++ src/compiler/scala/tools/nsc/transform/Erasure.scala
@@ -150,7 +150,7 @@ abstract class Erasure extends AddInterfaces
         case mt @ MethodType(params, restpe) =>
           MethodType(
             cloneSymbols(params) map (p => p.setInfo(apply(p.tpe))),
-            if (restpe.typeSymbol == UnitClass) 
+            if (restpe.typeSymbol == UnitClass)
               erasedTypeRef(UnitClass) 
             else if (settings.YdepMethTpes.value)
               // this replaces each typeref that refers to an argument by the type `p.tpe` of the actual argument p (p in params)
@@ -766,15 +766,15 @@ abstract class Erasure extends AddInterfaces
       while (opc.hasNext) {
         val member = opc.overriding
         val other = opc.overridden
-        //Console.println("bridge? " + member + ":" + member.tpe + member.locationString + " to " + other + ":" + other.tpe + other.locationString);//DEBUG
+        //Console.println("bridge? " + member + ":" + member.tpe + member.locationString + " to " + other + ":" + other.tpe + other.locationString)//DEBUG
         if (atPhase(currentRun.explicitouterPhase)(!member.isDeferred)) {
-          val otpe = erasure(other.tpe);
+          val otpe = erasure(other.tpe)
           val bridgeNeeded = atPhase(phase.next) (
             !(other.tpe =:= member.tpe) &&
             !(deconstMap(other.tpe) =:= deconstMap(member.tpe)) &&
             { var e = bridgesScope.lookupEntry(member.name)
               while ((e ne null) && !((e.sym.tpe =:= otpe) && (bridgeTarget(e.sym) == member)))
-                e = bridgesScope.lookupNextEntry(e);
+                e = bridgesScope.lookupNextEntry(e)
               (e eq null)
             }
           );
@@ -800,7 +800,7 @@ abstract class Erasure extends AddInterfaces
                     DefDef(bridge,
                       member.tpe match {
                         case MethodType(List(), ConstantType(c)) => Literal(c)
-                        case _ => 
+                        case _ =>
                           (((Select(This(owner), member): Tree) /: bridge.paramss)
                              ((fun, vparams) => Apply(fun, vparams map Ident)))
                       });
@@ -808,7 +808,7 @@ abstract class Erasure extends AddInterfaces
                     log("generating bridge from " + other + "(" + Flags.flagsToString(bridge.flags)  + ")" + ":" + otpe + other.locationString + " to " + member + ":" + erasure(member.tpe) + member.locationString + " =\n " + bridgeDef);
                   bridgeDef
                 }
-              } :: bridges;
+              } :: bridges
           }
         }
         opc.next
