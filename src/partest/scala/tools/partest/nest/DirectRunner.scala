02a1b235766af179987f9d7a6570dc5d750c16dc
extempore <extempore@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2011-03-22 04:28:21 +0000

    Not yet learned my lesson about partest and empty directories. Rather than reapply that bandaid, went after partest.  Attempts to make partest ignore empty directories.  Discover directory tests aren't run when the command line tool is used, make them run like everyone else.  Find more tests which due to misplacement are silently ignored, move them into tested locations.  No review.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@24536 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/partest/scala/tools/partest/nest/DirectRunner.scala
+++ src/partest/scala/tools/partest/nest/DirectRunner.scala
@@ -25,6 +25,20 @@ trait DirectRunner {
   def fileManager: FileManager
   
   import PartestDefaults.numActors
+  
+  def denotesTestFile(arg: String) = Path(arg).hasExtension("scala", "res")
+  def denotesTestDir(arg: String)  = Path(arg).ifDirectory(_.files.nonEmpty) exists (x => x)
+  def denotesTestPath(arg: String) = denotesTestDir(arg) || denotesTestFile(arg)
+  
+  /** No duplicate, no empty directories, don't mess with this unless
+   *  you like partest hangs.
+   */
+  def onlyValidTestPaths[T](args: List[T]): List[T] = {
+    args.distinct filter (arg => denotesTestPath("" + arg) || {
+      NestUI.warning("Discarding invalid test path '%s'\n" format arg)
+      false
+    })
+  }
 
   def setProperties() {
     if (isPartestDebug)
@@ -37,8 +51,7 @@ trait DirectRunner {
   }
 
   def runTestsForFiles(_kindFiles: List[File], kind: String): immutable.Map[String, Int] = {
-    /** NO DUPLICATES, or partest will blow the count and hang forever. */
-    val kindFiles = _kindFiles.distinct
+    val kindFiles = onlyValidTestPaths(_kindFiles)
     val groupSize = (kindFiles.length / numActors) + 1
 
     val consFM = new ConsoleFileManager
