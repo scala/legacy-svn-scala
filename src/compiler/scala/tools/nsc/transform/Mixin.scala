5e7f55750e5dd55318e320f2893418777541e14b
plocinic <plocinic@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2011-04-12 17:12:29 +0000

    closes #4426. name mangling for private lazy vals that are lifted during explicitouter is now taken into account. this also handles lazy vals/objects inside anonymous classes. review by dragos.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@24743 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/tools/nsc/transform/Mixin.scala
+++ src/compiler/scala/tools/nsc/transform/Mixin.scala
@@ -675,7 +675,7 @@ abstract class Mixin extends InfoTransform with ast.TreeDSL {
       def bitmapOperation[T](field: Symbol, transientCase: => T, privateCase: => T, rest: => T): T =
         if (field.accessed.hasAnnotation(TransientAttr))
           transientCase
-        else if (field.hasFlag(PRIVATE))
+        else if (field.hasFlag(PRIVATE) || field.hasFlag(notPRIVATE))
           privateCase
         else
           rest
@@ -686,7 +686,7 @@ abstract class Mixin extends InfoTransform with ast.TreeDSL {
        *  Similarly fields in the checkinit mode use private bitmaps.
        */
       def localBitmapField(field: Symbol) =
-        field.accessed.hasAnnotation(TransientAttr) || field.hasFlag(PRIVATE) || checkinitField(field)
+        field.accessed.hasAnnotation(TransientAttr) || field.hasFlag(PRIVATE | notPRIVATE) || checkinitField(field)
 
       /**
        *  Return the bitmap field for 'offset'. Depending on the hierarchy it is possible to reuse
@@ -705,7 +705,6 @@ abstract class Mixin extends InfoTransform with ast.TreeDSL {
         val bitmapName = if (checkinitField) bitmapCheckinitName else bitmapLazyName
  
         def createBitmap: Symbol = {
-
           val sym = clazz0.newVariable(clazz0.pos, bitmapName).setInfo(IntClass.tpe)
           atPhase(currentRun.typerPhase) {
             sym addAnnotation AnnotationInfo(VolatileAttr.tpe, Nil, Nil)
@@ -963,7 +962,7 @@ abstract class Mixin extends InfoTransform with ast.TreeDSL {
           field <- cl.info.decls.iterator
           if needsBitmap(field) && !localBitmapField(field)
         } bits += 1
-        
+
         bits
       }
       
@@ -980,7 +979,7 @@ abstract class Mixin extends InfoTransform with ast.TreeDSL {
 
         for (f <- clazz0.info.decls.iterator) {
           if (settings.debug.value) log(f.fullName + " -> " + fields)
-          
+
           if (fieldWithBitmap(f)) {
             val (idx, _) =
               bitmapOperation(f, (fieldsTransient, fieldsTransient += 1),
