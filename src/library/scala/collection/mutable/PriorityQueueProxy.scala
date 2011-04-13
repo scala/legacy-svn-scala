9c79f6b6cc5c4247c47882669d6159c5bd983da5
prokopec <prokopec@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2011-04-13 16:32:04 +0000

    Further fixes #4405.
    No review.
    
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@24755 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/library/scala/collection/mutable/PriorityQueueProxy.scala
+++ src/library/scala/collection/mutable/PriorityQueueProxy.scala
@@ -67,12 +67,20 @@ abstract class PriorityQueueProxy[A](implicit ord: Ordering[A]) extends Priority
    *  @return   the element with the highest priority.
    */
   override def dequeue(): A = self.dequeue
-
+  
   /** Returns the element with the highest priority in the queue,
    *  or throws an error if there is no element contained in the queue.
    *
    *  @return   the element with the highest priority.
    */
+  override def head: A = self.head
+  
+  /** Returns the element with the highest priority in the queue,
+   *  or throws an error if there is no element contained in the queue.
+   *
+   *  @return   the element with the highest priority.
+   */
+  @deprecated("Use `head` instead.")
   override def max: A = self.max
 
   /** Removes all elements from the queue. After this operation is completed,
