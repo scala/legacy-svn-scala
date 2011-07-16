0bca4da37342f399449dee19c767f3807bbc77e2
extempore <extempore@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2011-07-16 20:40:11 +0000

    Removed classes and methods deprecated before 2.8.
    Contributed by Simon Ochsenreither.
    
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@25317 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/actors/scala/actors/scheduler/TerminationMonitor.scala
+++ src/actors/scala/actors/scheduler/TerminationMonitor.scala
@@ -63,10 +63,6 @@ private[scheduler] trait TerminationMonitor {
   private[actors] def allActorsTerminated: Boolean = synchronized {
     started && activeActors <= 0
   }
-  
-  /** Deprecated non-actor-private version */
-  @deprecated("this method is going to be removed in a future release", "2.7.7")
-  def allTerminated: Boolean = allActorsTerminated
 
   /** Checks for actors that have become garbage. */
   protected def gc() {}
