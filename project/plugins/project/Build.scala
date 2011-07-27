057681580651af5cae2468086627a8456c21468a
jsuereth <Joshua.Suereth@gmail.com>
2011-07-27 16:59:46 -0400

    Fixed issue with proguard

--- project/plugins/project/Build.scala
+++ project/plugins/project/Build.scala
@@ -2,5 +2,7 @@ import sbt._
 object PluginDef extends Build {
   override def projects = Seq(root)
   lazy val root = Project("plugins", file(".")) dependsOn(proguard)
-  lazy val proguard = uri("git://github.com/siasia/xsbt-proguard-plugin.git")
+  lazy val proguard = uri("git://github.com/jsuereth/xsbt-proguard-plugin.git")
+  // TODO - fix the above when able!
+  //uri("git://github.com/siasia/xsbt-proguard-plugin.git")
 }
