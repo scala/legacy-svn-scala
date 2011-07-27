75f265a0ef02423b018d9c0184e701522836711e
jsuereth <Joshua.Suereth@gmail.com>
2011-07-27 16:46:29 -0400

    Migrated things around to clean them up.  This wont' compile until xsbt-proguard's public repo is ready

--- build.sbt
+++ build.sbt
@@ -1,9 +1,11 @@
 // resolvers += ScalaToolsSnapshots
 
-libraryDependencies += "org.scala-tools.testing" %% "scalacheck" % "1.9" % "test"
+//libraryDependencies += "org.scala-tools.testing" %% "scalacheck" % "1.9" % "test"
 
-libraryDependencies += "org.scala-lang" % "jline" % "2.9.0-1"
-
-libraryDependencies += "org.apache.ant" % "ant" % "1.8.2"
+//libraryDependencies += "org.apache.ant" % "ant" % "1.8.2"
 
 // retrieveManaged := true
+
+version := "2.10.1-SNAPSHOT"
+
+scalaVersion := "2.9.0-1"
--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object ScalaBuild extends Build {
   // lazy val projects  = Seq(root, compQuick, libQuick)
-  lazy val root      = Project("scala", file(".")) aggregate(compQuick)
+  lazy val root      = Project("scala", file(".")) aggregate(lockerComp)
 
   // --------------------------------------------------------------
   //  Libraries used by Scalac that change infrequently
@@ -31,46 +31,45 @@ object ScalaBuild extends Build {
                             javacOptions ++= Seq("-target", "1.5",
                                                  "-source", "1.4")))
 
+  import LayeredBuild._
+
   // --------------------------------------------------------------
   //  The magic kingdom.
   //  Layered compilation of Scala.
   //   Stable Reference -> Locker ('Lockable' dev version) -> Quick -> Strap (Binary compatibility testing)
   // --------------------------------------------------------------
 
-  
-
-	lazy val compQuick = compiler(libQuick, "quick")
-	lazy val libQuick  = library("quick") settings (
-	  classpathOptions := ClasspathOptions.boot
-    // classpathOptions := ClasspathOptions(bootLibrary = true, compiler = false, extra = false, autoBoot = true, filterLibrary = true)
-	)
-
-	def compiler(lib: ProjectReference, stage: String): Project =
-		Project("compiler-" + stage, file(".")) dependsOn(lib) aggregate(lib) settings(compilerSettings(stage) : _*)
-
-	def library(stage: String): Project =
-		Project("library-" + stage, file(".")) settings(librarySettings(stage) : _*)
-
-	def commonSettings(stage: String, component: String): Seq[Setting[_]] = Seq(
-		target <<= target(_ / stage / component),
-		crossPaths := false,
-		src(component),
-		classpathOptions := ClasspathOptions.manual,
-		sourceDirectory in Compile <<= baseDirectory(_ / "src"),
-		version := "2.10.1-SNAPSHOT",
-		scalaVersion := "2.9.0-1"
-	)
-	def librarySettings(stage: String) = commonSettings(stage, "library") ++ Seq(
-		unmanaged(lib => Seq(lib / "fjbg.jar", lib / "jline.jar"))
-	)
-	def compilerSettings(stage: String) = commonSettings(stage, "compiler") ++ Seq(
-		unmanaged(lib => Seq(lib / "fjbg.jar", lib / "jline.jar", lib /"ant" / "ant.jar", lib / "msil.jar")),
-		mainClass in (Compile,run) := Some("scala.tools.nsc.MainGenericRunner")
-	)
-
-	def src(name: String) =
-		scalaSource in Compile <<= sourceDirectory(_ / name)
-
-	def unmanaged(files: File => Seq[File]) =
-		unmanagedClasspath in Compile <<= unmanagedBase map { base => files(base) map Attributed.blank }
+    lazy val lockerLib = Project("locker-library", file("."), settings = layeredProjectSettings("quick") ++
+    Seq(name := "scala-library",
+        layerProjectName := "library",
+        version := "quick",
+        dependencyClasspath := Seq(),
+        scalaInstance <<= appConfiguration map { app =>
+		      val launcher = app.provider.scalaProvider.launcher
+          // TODO - This is STARR, reference STARR using dependency resolution
+          // TODO - Explicitly version STARR
+          ScalaInstance(
+            file("lib/scala-library.jar"),
+            file("lib/scala-compiler.jar"),
+            launcher,
+            file("lib/fjbg.jar"))
+        }
+    )
+  )
+  lazy val lockerComp = Project("locker-compiler", file("."), settings = layeredProjectSettings("quick") ++
+    Seq(name := "scala-compiler",
+        layerProjectName := "compiler",
+        version := "locker",
+        dependencyClasspath := Seq(),
+        scalaInstance <<= (appConfiguration, classDirectory in lockerLib) map { (app, lib) =>
+		      val launcher = app.provider.scalaProvider.launcher
+          // TODO - Figure out dependency resoltuion to look up STARR and only use compiler *not* library...
+          ScalaInstance(
+            lib,
+            file("lib/scala-compiler.jar"),
+            launcher,
+            file("lib/fjbg.jar"))
+        }
+    )
+  ) dependsOn(lockerLib)
 }
