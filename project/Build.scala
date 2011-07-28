e2643e76214ccf88d2fbb42e65f897c2204c3558
jsuereth <Joshua.Suereth@gmail.com>
2011-07-28 15:15:34 -0400

    Fixes to get locker-library to build.  For now it builds off of ant's locker.done task

--- project/Build.scala
+++ project/Build.scala
@@ -1,9 +1,9 @@
 import sbt._
 import Keys._
 
-object ScalaBuild extends Build {
+object ScalaBuild extends LayeredBuild {
   // lazy val projects  = Seq(root, compQuick, libQuick)
-  lazy val root      = Project("scala", file(".")) aggregate(lockerComp)
+  lazy val root      = Project("scala", file(".")) aggregate(lockerLib)
 
   // --------------------------------------------------------------
   //  Libraries used by Scalac that change infrequently
@@ -12,32 +12,37 @@ object ScalaBuild extends Build {
 
 
   // Jline nested project.   Compile this sucker once and be done.
-  lazy val jline = Project("jline", file("src/jline"))
+  override lazy val jline = Project("jline", file("src/jline"))
   // Fast Java Bytecode Generator (nested in every scala-compiler.jar)
-  lazy val fjbg = Project("fjbg", file("src/fjbg"),
+  override lazy val fjbg = Project("fjbg", file("."),
                           settings = Defaults.defaultSettings ++ Seq(
                             javaSource in Compile := file("src/fjbg"),
-                            javacOptions ++= Seq("-target", "1.5")))
+                            javacOptions ++= Seq("-target", "1.5"),
+                            target := file("target/fjbg"),
+                            (classDirectory in Compile) <<= target(_ / "classes")))
 
   // Forkjoin backport
-  lazy val forkjoin = Project("forkjoin", file("src/forkjoin"),
+  override lazy val forkjoin = Project("forkjoin", file("."),
                           settings = Defaults.defaultSettings ++ Seq(
                             javaSource in Compile := file("src/forkjoin"),
-                            javacOptions ++= Seq("-target", "1.5")))
+                            javacOptions ++= Seq("-target", "1.5"),  
+                            target := file("target/forkjoin"),                          
+                            (classDirectory in Compile) <<= target(_ / "classes")))
 
   // MSIL code generator
   // TODO - This probably needs to compile against quick, but Sabbus
   // had it building against locker, so we'll do worse and build
   // build against STARR for now.
-  lazy val msil = Project("msil", file("src/msil"),
+  lazy val msil = Project("msil", file("."),
                           settings = Defaults.defaultSettings ++ Seq(
                             javaSource in Compile := file("src/msil"),
                             scalaSource in Compile := file("src/msil"),
                             defaultExcludes ~= (_ || "tests"),
                             javacOptions ++= Seq("-target", "1.5",
-                                                 "-source", "1.4")))
+                                                 "-source", "1.4"),
+                            target := file("target/msil"),                          
+                            (classDirectory in Compile) <<= target(_ / "classes")))
 
-  import LayeredBuild._
 
   // --------------------------------------------------------------
   //  The magic kingdom.
@@ -45,38 +50,51 @@ object ScalaBuild extends Build {
   //   Stable Reference -> Locker ('Lockable' dev version) -> Quick -> Strap (Binary compatibility testing)
   // --------------------------------------------------------------
 
-    lazy val lockerLib = Project("locker-library", file("."), settings = layeredProjectSettings("quick") ++
+  lazy val (lockerLib, lockerComp) = makeLayer("locker", file("build/locker/classes/library"), file("build/locker/classes/compiler"))
+
+/*
+    lazy val lockerLib = Project("locker-library", file("."), settings = layeredProjectSettings("locker") ++
     Seq(name := "scala-library",
         layerProjectName := "library",
-        version := "quick",
-        dependencyClasspath <<= (classDirectory in forkjoin in Compile, classDirectory in Compile, scalaInstance) map { 
-          (fj, cur, scala) => Seq(fj, cur, scala.libraryJar, scala.compilerJar) map Attributed.blank },
+        version := "locker",
+        // TODO - use depends on.
+        dependencyClasspath <<= (classDirectory in forkjoin in Compile) map { 
+          (fj) => Seq(fj) map Attributed.blank },
         scalaInstance <<= appConfiguration map { app =>
-		      val launcher = app.provider.scalaProvider.launcher
+          val launcher = app.provider.scalaProvider.launcher
           // TODO - This is STARR, reference STARR using dependency resolution
           // TODO - Explicitly version STARR
           ScalaInstance(
-            file("lib/scala-library.jar"),
-            file("lib/scala-compiler.jar"),
+            file("build/locker/classes/library"),
+            file("build/locker/classes/compiler"),
             launcher,
             file("lib/fjbg.jar"))
         }
     )
-  ) dependsOn(forkjoin)
-  lazy val lockerComp = Project("locker-compiler", file("."), settings = layeredProjectSettings("quick") ++
+  ) aggregate(forkjoin)
+  lazy val lockerComp = Project("locker-compiler", file("."), settings = layeredProjectSettings("locker") ++
     Seq(name := "scala-compiler",
         layerProjectName := "compiler",
         version := "locker",
-        dependencyClasspath := Seq(),
+        // TODO - Use depends on *and* SBT's magic dependency mechanisms...
+        dependencyClasspath <<= (classDirectory in forkjoin in Compile,
+                                 classDirectory in lockerLib in Compile,
+                                 classDirectory in fjbg in Compile,
+                                 classDirectory in jline in Compile,
+                                 classDirectory in msil in Compile) map {
+          (fj, lib, fjbg, jline, msil) =>
+            Seq(fj, lib, fjbg, jline, msil, file("lib/ant/ant.jar")) map Attributed.blank
+        },
         scalaInstance <<= (appConfiguration, classDirectory in lockerLib) map { (app, lib) =>
 		      val launcher = app.provider.scalaProvider.launcher
           // TODO - Figure out dependency resoltuion to look up STARR and only use compiler *not* library...
           ScalaInstance(
-            lib,
-            file("lib/scala-compiler.jar"),
+            file("build/locker/classes/library"),
+            file("build/locker/classes/compiler"),
             launcher,
             file("lib/fjbg.jar"))
         }
     )
-  ) dependsOn(lockerLib, fjbg) aggregate(fjbg)
+  ) aggregate(fjbg, jline, lockerLib)
+  */
 }
--- project/LayeredBuild.scala
+++ project/LayeredBuild.scala
@@ -2,21 +2,28 @@ import sbt._
 import Keys._
 
 
-object LayeredBuild {
+trait LayeredBuild extends Build {
+  // All the dependencies to make layered builds..
+  def fjbg : Project
+  def jline : Project
+  // TODO - This probably needs to be defined here as well, since it has Scala source code *and* java source code...
+  def msil : Project
+  def forkjoin : Project
+
   import Build._
 
   def compileInputsTask =
-    (dependencyClasspath, sources, compilers, classDirectory, compileIncSetup, streams) map {
-    (cp, srcs, cs, classes, incSetup, s) =>
+    (dependencyClasspath, sources, compilers, classDirectory, compileIncSetup, streams, sourceDirectory) map {
+    (cp, srcs, cs, classes, incSetup, s, sd) =>
 			val classpath = classes +: data(cp)
       // TODO - Ask mark to use default parameters so named arguments work here..
-      Compiler.inputs(classpath,                               // Classpath
-                      srcs,                                    // Sources
-                      classes,                                 // Output Directory
-                      Seq[String](),                           // Scalac Options
-                      Seq[String]("-target", "1.5"),           // Javac Options
-                      100,                                     // Max Errors
-                      CompileOrder.JavaThenScala)(                     // Compilation Order
+      Compiler.inputs(classpath,                                      // Classpath
+                      sortFiles(srcs),                                // Sources
+                      classes,                                        // Output Directory
+                      Seq[String]("-sourcepath", sd.getAbsolutePath), // TODO - Allow Scalac Options
+                      Seq[String]("-target", "1.5"),                  // TODO - Allow Javac Options
+                      100,                                            // Max Errors
+                      CompileOrder.JavaThenScala)(                    // Compilation Order
                       cs, incSetup, s.log)
   }
   // TODO - Figure out WTF to do here...
@@ -28,19 +35,33 @@ object LayeredBuild {
   def compilersSetting = compilers <<= (scalaInstance, appConfiguration, streams, classpathOptions, javaHome) map {
     (si, app, s, co, jh) => Compiler.compilers(si, co, jh)(app, s.log)
   }
+  def sortFiles(files : Seq[File]) = files sortWith { (lhs, rhs) =>
+    (lhs.getAbsolutePath, rhs.getAbsolutePath) match {
+      case (lhs, rhs) if (lhs endsWith "package.scala") && (rhs endsWith "package.scala") => lhs < rhs
+      case (lhs, rhs) if lhs endsWith "package.scala" => true
+      case (lhs, rhs) if rhs endsWith "package.scala" => false
+      case (lhs, rhs) => lhs < rhs
+    }    
+  }
   val layerProjectName = SettingKey[String]("layer-project-name", "The name of the project for layered building.")
   def paths(layer : String): Seq[Setting[_]] = Seq(
 		baseDirectory <<= thisProject(_.base),
-		target <<= baseDirectory / ("target/" + layer),
+		target <<= (baseDirectory, layerProjectName) apply { (bd, name) => bd / "target" / layer / name },
 		defaultExcludes in GlobalScope :== (".*"  - ".") || HiddenFileFilter,
 		historyPath <<= target(t => Some(t / ".history")),
     // TODO - move this to src/library src/compiler etc.
 		sourceDirectory <<= (baseDirectory, layerProjectName) { _ / "src" / _ },
 		sourceFilter in GlobalScope :== ("*.java" | "*.scala"),
 		cacheDirectory <<= target / "cache",
-    classDirectory <<= (target, name){(t,n) => t / n / "classes"}
+                classDirectory <<= target(_ / "classes")
 	)
-  def layeredProjectSettings(layer : String): Seq[Setting[_]] = paths(layer) ++ Seq[Setting[_]](
+  // Simple tasks, like clean.
+  lazy val projectTasks: Seq[Setting[_]] = Seq(
+		cleanFiles <<= Seq(target).join,
+		cleanKeepFiles <<= historyPath(_.toList),
+		clean <<= (cleanFiles, cleanKeepFiles) map Defaults.doClean)
+
+  def layeredProjectSettings(layer : String): Seq[Setting[_]] = paths(layer) ++ projectTasks ++ Seq[Setting[_]](
     compileInputs <<= compileInputsTask,
     compileIncSetup <<= compileIncSetupTask,
     unmanagedSourceDirectories <<= Seq(sourceDirectory).join,
@@ -49,4 +70,56 @@ object LayeredBuild {
     classpathOptions in GlobalScope :== ClasspathOptions.boot,
     compile <<= compileTask
   )
+
+  // Creates a 'layer' of a Scala build using a different instance of Scala.
+  def makeLayer(layer: String, scalaLibraryPath : File, scalaCompilerPath : File) : (Project, Project) = {
+    val library = Project(layer + "-library", file("."), settings = layeredProjectSettings(layer) ++
+      Seq(name := "scala-library",
+          layerProjectName := "library",
+          version := layer,
+          // TODO - use depends on.
+          dependencyClasspath <<= (classDirectory in forkjoin in Compile) map { 
+            (fj) => Seq(fj) map Attributed.blank },
+          scalaInstance <<= appConfiguration map { app =>
+            val launcher = app.provider.scalaProvider.launcher
+            // TODO - Pass in FJBG
+            // TODO - Explicitly version?
+            ScalaInstance(
+              scalaLibraryPath,
+              scalaCompilerPath,
+              launcher,
+              file("lib/fjbg.jar"))
+          }
+      )
+    ) aggregate(forkjoin)
+
+    // Define the compiler
+    val compiler = Project(layer + "-compiler", file("."), settings = layeredProjectSettings(layer) ++
+    Seq(name := "scala-compiler",
+        layerProjectName := "compiler",
+        version := layer,
+        // TODO - Use depends on *and* SBT's magic dependency mechanisms...
+        dependencyClasspath <<= (classDirectory in forkjoin in Compile,
+                                 classDirectory in library in Compile,
+                                 classDirectory in fjbg in Compile,
+                                 classDirectory in jline in Compile,
+                                 classDirectory in msil in Compile) map {
+          (fj, lib, fjbg, jline, msil) =>
+            Seq(fj, lib, fjbg, jline, msil, file("lib/ant/ant.jar")) map Attributed.blank
+        },
+        scalaInstance <<= (appConfiguration, classDirectory in library) map { (app, lib) =>
+		      val launcher = app.provider.scalaProvider.launcher
+          // TODO - explicitly pull FJBG from somewhere...
+          ScalaInstance(
+            scalaLibraryPath,
+            scalaCompilerPath,
+            launcher,
+            file("lib/fjbg.jar"))
+        }
+      )
+    ) aggregate(fjbg, jline, library, msil)
+
+    // Return the generated projects.
+    (library, compiler)
+  }
 }
