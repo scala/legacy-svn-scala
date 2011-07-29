6b5fdd4f2c381af3d3f251ff0e994d362e05ec1f
jsuereth <Joshua.Suereth@gmail.com>
2011-07-29 11:47:46 -0400

    Fixed sourcepath issue

--- project/Build.scala
+++ project/Build.scala
@@ -99,6 +99,8 @@ object ScalaBuild extends Build {
           // TODO - use depends on.
           unmanagedClasspath in Compile <<= (exportedProducts in forkjoin in Compile).identity,
           scalaSource in Compile <<= (baseDirectory) apply (_ / "src" / "library"),
+          // TODO - Allow other scalac option settings.
+          scalacOptions in Compile <<= (scalaSource in Compile) map (src => Seq("-sourcepath", src.getAbsolutePath)),
           referenceScala
       )) :_*)
 
