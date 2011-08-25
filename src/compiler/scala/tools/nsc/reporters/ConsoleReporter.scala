6a0290fa35c39d58d513e85871233399d8385149
extempore <extempore@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2011-08-25 17:33:37 +0000

    Made -Xprompt more useful, less crashy and more open to showing you the stack trace without aborting.  For example, the following would resume compilation after showing the trace.
      % scala -deprecation -Xprompt -nc -e 'case class Foo'
      foo.scala:1: warning: case classes without a parameter list have been deprecated;
      use either case objects or case classes with `()' as parameter list.
      case class Foo
                   ^
    
      a)bort, s)tack, r)esume: s
      java.lang.Exception
        [...]
      	at scala.tools.nsc.ast.parser.Parsers$UnitParser.deprecationWarning(Parsers.scala:207)
      	at scala.tools.nsc.ast.parser.Parsers$Parser.paramClauses(Parsers.scala:2058)
      	at scala.tools.nsc.ast.parser.Parsers$Parser$$anonfun$classDef$1.apply(Parsers.scala:2557)
      	at scala.tools.nsc.ast.parser.Parsers$Parser$$anonfun$classDef$1.apply(Parsers.scala:2545)
    
    No review.
    
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@25567 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/tools/nsc/reporters/ConsoleReporter.scala
+++ src/compiler/scala/tools/nsc/reporters/ConsoleReporter.scala
@@ -16,7 +16,7 @@ import scala.tools.util.StringOps.countElementsAsString
  */
 class ConsoleReporter(val settings: Settings, reader: BufferedReader, writer: PrintWriter) extends AbstractReporter {
   def this(settings: Settings) = this(settings, Console.in, new PrintWriter(Console.err, true))
-  
+
   /** Whether a short file name should be displayed before errors */
   var shortname: Boolean = false
 
@@ -78,9 +78,9 @@ class ConsoleReporter(val settings: Settings, reader: BufferedReader, writer: Pr
    *
    *  @param pos ...
    */
-  def printColumnMarker(pos: Position) = 
+  def printColumnMarker(pos: Position) =
     if (pos.isDefined) { printMessage(" " * (pos.column - 1) + "^") }
-  
+
   /** Prints the number of errors and warnings if their are non-zero. */
   def printSummary() {
     if (WARNING.count > 0) printMessage(getCountString(WARNING) + " found")
@@ -93,25 +93,19 @@ class ConsoleReporter(val settings: Settings, reader: BufferedReader, writer: Pr
       print(pos, msg, severity)
   }
 
-  def displayPrompt(): Unit = try {
-    var continue = true
-    while (continue) {
-      writer.print("r)esume, a)bort: ")
-      writer.flush()
-      var line = reader.readLine()
-      if (line ne null) {
-	      line = line.toLowerCase()
-	      if ("abort" startsWith line)
-          abort("user abort")
-	      if ("resume" startsWith line)
-	        continue = false
+  def displayPrompt(): Unit = {
+    writer.print("\na)bort, s)tack, r)esume: ")
+    writer.flush()
+    if (reader != null) {
+      val response = reader.read().asInstanceOf[Char].toLower
+      if (response == 'a' || response == 's') {
+        (new Exception).printStackTrace()
+        if (response == 'a')
+          sys exit 1
+
+        writer.print("\n")
+        writer.flush()
       }
-    }
-  } 
-  catch {
-    case ex: IOException => {
-      ex.printStackTrace()
-      abort("input read error")
     }
   }
 
