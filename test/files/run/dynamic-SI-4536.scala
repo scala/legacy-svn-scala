4e8f0118ab82af1c9c549c3f80dd6f3011718f8a
Jordi Salvat i Alabart <jordi.salvat.i.alabart@gmail.com>
2011-10-01 07:19:59 +0200

    Additional tests for scala.Dynamic.

---
+++ test/files/run/dynamic-SI-4536.scala
@@ -0,0 +1,30 @@
+object Test extends App with Dynamic {
+  def applyDynamic(m: String)(a: Any*) = "DynamicClass."+m+"("+a+")"
+  def regularMethod() = "Regular method"
+  println(this.bar)
+  println(this.bar())
+  println(Test.bar())
+  println(this.regularMethod)
+  println(this.regularMethod())
+  println(Test.regularMethod())
+}
+object Test extends App with Dynamic {
+  def applyDynamic(m: String)(a: Any*) = "DynamicClass."+m+"("+a+")"
+  def regularMethod() = "Regular method"
+  println(this.bar)
+  println(this.bar())
+  println(Test.bar())
+  println(this.regularMethod)
+  println(this.regularMethod())
+  println(Test.regularMethod())
+}
+object Test extends App with Dynamic {
+  def applyDynamic(m: String)(a: Any*) = "DynamicClass."+m+"("+a+")"
+  def regularMethod() = "Regular method"
+  println(this.bar)
+  println(this.bar())
+  println(Test.bar())
+  println(this.regularMethod)
+  println(this.regularMethod())
+  println(Test.regularMethod())
+}
