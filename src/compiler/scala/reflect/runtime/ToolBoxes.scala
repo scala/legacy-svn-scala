335f313e30a730b99f45efe619d84a9d302fd1f0
odersky <odersky@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2011-10-21 16:05:48 +0000

    Towards runtime code generation. No review.
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@25869 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/reflect/runtime/ToolBoxes.scala
+++ src/compiler/scala/reflect/runtime/ToolBoxes.scala
@@ -14,6 +14,7 @@ import scala.tools.nsc.util.FreshNameCreator
 import scala.reflect.internal.Flags
 import scala.tools.nsc.util.NoSourceFile
 import java.lang.{Class => jClass}
+import scala.tools.nsc.util.trace
 
 trait ToolBoxes extends { self: Universe =>
 
@@ -46,7 +47,7 @@ trait ToolBoxes extends { self: Universe =>
       }
       
       def compileExpr(expr: Tree, fvs: List[Symbol]): String = {
-        val cdef = wrapInClass(expr, fvs)
+        val cdef = trace("wrapped: ")(wrapInClass(expr, fvs))
         val unit = wrapInCompilationUnit(cdef)
         val run = new Run
         run.compileUnits(List(unit), run.namerPhase)
@@ -77,18 +78,22 @@ trait ToolBoxes extends { self: Universe =>
     lazy val exporter = importer.reverse
     
     lazy val classLoader = new AbstractFileClassLoader(virtualDirectory, getClass.getClassLoader) 
-    
 
-    def typeCheck(tree: rm.Tree, expectedType: rm.Type): rm.Tree = {
-      if (compiler.settings.verbose.value) println("typing "+tree+", pt = "+expectedType)
-      val run = new compiler.Run
-      compiler.phase = run.refchecksPhase
+    private def importAndTypeCheck(tree: rm.Tree, expectedType: rm.Type): compiler.Tree = {
       val ctree: compiler.Tree = importer.importTree(tree.asInstanceOf[Tree])
       val pt: compiler.Type = importer.importType(expectedType.asInstanceOf[Type])
+      val run = new compiler.Run
+      compiler.phase = run.refchecksPhase
       val ttree: compiler.Tree = compiler.typer.typed(ctree, compiler.analyzer.EXPRmode, pt)
+      ttree
+    } 
+      
+    def typeCheck(tree: rm.Tree, expectedType: rm.Type): rm.Tree = {
+      if (compiler.settings.verbose.value) println("typing "+tree+", pt = "+expectedType)
+      val ttree = importAndTypeCheck(tree, expectedType)
       exporter.importTree(ttree).asInstanceOf[rm.Tree]
     }
-    
+      
     def typeCheck(tree: rm.Tree): rm.Tree = 
       typeCheck(tree, WildcardType.asInstanceOf[rm.Type])
 
@@ -100,5 +105,12 @@ trait ToolBoxes extends { self: Universe =>
       } finally 
         compiler.settings.printtypes.value = saved
     }
+    
+    def runExpr(tree: rm.Tree, expectedType: rm.Type): Any = {
+      val ttree = importAndTypeCheck(tree, expectedType)
+      compiler.runExpr(ttree)
+    }
+    
+    def runExpr(tree: rm.Tree): Any = runExpr(tree, WildcardType.asInstanceOf[rm.Type])
   }
 }
