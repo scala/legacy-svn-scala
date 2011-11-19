cebf91527bde1a630f6cfd4ab2b88913f9390bdf
extempore <extempore@5e8d7ff9-d8ef-0310-90f0-a4852d11357a>
2011-11-19 06:59:19 +0000

    Bringing a bit of order to symbol substitution.
    Painstakingly winnowed out the most frequently duplicated code
    sequences related to symbol cloning and substitution.  Created
    canonical methods to perform these actions and documented them.
    Key methods include:
    
      def createFromClonedSymbols[T](syms: List[Symbol], tpe: Type)(creator: (List[Symbol], Type) => T): T
      def deriveSymbols(syms: List[Symbol], symFn: Symbol => Symbol): List[Symbol]
      def deriveType(syms: List[Symbol], symFn: Symbol => Symbol)(tpe: Type): Type
    
    Many example usages enclosed with commit.
    
    I did lots of timing tests, I find no material difference before and
    after. Actually I won by four seconds in this incarnation:
    
      Before - Total time: 7 minutes 55 seconds
       After - Total time: 7 minutes 51 seconds
    
    Review by moors.
    
    git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@26032 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

--- src/compiler/scala/tools/nsc/typechecker/RefChecks.scala
+++ src/compiler/scala/tools/nsc/typechecker/RefChecks.scala
@@ -1562,15 +1562,12 @@ abstract class RefChecks extends InfoTransform with reflect.internal.transform.R
             }
 
             val existentialParams = new ListBuffer[Symbol]
-            doTypeTraversal(tree) { // check all bounds, except those that are
-                              // existential type parameters
+            doTypeTraversal(tree) { // check all bounds, except those that are existential type parameters
               case ExistentialType(tparams, tpe) => 
                 existentialParams ++= tparams
               case t: TypeRef => 
-                val exparams = existentialParams.toList
-                val wildcards = exparams map (_ => WildcardType)
-                checkTypeRef(t.subst(exparams, wildcards), tree.pos)
-              case _ => 
+                checkTypeRef(deriveTypeWithWildcards(existentialParams.toList)(t), tree.pos)
+              case _ =>
             }
             tree
 
